ed3df9e07e2bfd1f2217eb63775216787f93e21c
Rene Dohan <dohan.rene@gmail.com>
2022-06-26 21:44:52 -0500

    ..

---
+++ documentation/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":renetik-android-core:dokkaHtml/androidTestRelease":[],":renetik-android-core:dokkaHtml/debug":[],":renetik-android-core:dokkaHtml/main":[],":renetik-android-core:dokkaHtml/release":[],":renetik-android-core:dokkaHtml/testFixtures":[],":renetik-android-core:dokkaHtml/testFixturesDebug":[],":renetik-android-core:dokkaHtml/testFixturesRelease":[]}'
