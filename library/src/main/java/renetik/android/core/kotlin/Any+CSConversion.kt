c0308016962af6be20e0c18e122d817e87dcb081
Rene Dohan <dohan.rene@gmail.com>
2022-06-26 19:36:56 -0500

    ...

--- library/src/main/java/renetik/android/core/kotlin/Any+CSConversion.kt
+++ library/src/main/java/renetik/android/core/kotlin/Any+CSConversion.kt
@@ -1,24 +1,30 @@
 package renetik.android.core.kotlin
 
-import renetik.android.core.kotlin.primitives.isEmpty
+import renetik.android.core.kotlin.primitives.asDouble
+import renetik.android.core.kotlin.primitives.asFloat
+import renetik.android.core.kotlin.primitives.asInt
+import renetik.android.core.kotlin.primitives.asLong
 import renetik.android.core.lang.CSHasTitle
 import renetik.android.core.lang.CSValue
-import renetik.android.core.lang.catchAllWarnReturn
 
 @Suppress("UNCHECKED_CAST")
 fun <Type : Any> Any.type() = (this as Type)
 
+//val Any.asString: String
+//	get() = (this as? CSHasTitle)?.title
+//		?: (this as? CSValue<*>)?.value as? String
+//		?: "$this"
+
 val Any?.asString
-    get() = (this as? CSHasTitle)?.title
-        ?: (this as? CSValue<*>)?.value as? String
-        ?: this?.let { "$it" } ?: ""
+	get() = (this as? CSHasTitle)?.title
+		?: (this as? CSValue<*>)?.value as? String
+		?: this?.let { "$it" } ?: ""
 
 val List<*>.asStringArray get() = asStringList.toTypedArray()
-
 val List<*>.asStringList: List<String> get() = map { it.asString }
 
-val Any?.asInt
-    get() = catchAllWarnReturn(0) {
-        val value = asString
-        if (value.isEmpty) 0 else value.toInt()
-    }
+fun Any.asInt(): Int? = asString.asInt()
+fun Any.asLong(): Long? = asString.asLong()
+fun Any.asFloat(): Float? = asString.asFloat()
+fun Any.asDouble(): Double? = asString.asDouble()
+fun Any.asBoolean(): Boolean = asString.toBoolean()
--- library/src/main/java/renetik/android/core/kotlin/primitives/String+.kt
+++ library/src/main/java/renetik/android/core/kotlin/primitives/String+.kt
@@ -1,12 +1,12 @@
 package renetik.android.core.kotlin.primitives
 
-import renetik.android.core.lang.catchWarnReturn
-import renetik.android.core.lang.catchWarnReturnNull
 import renetik.android.core.kotlin.notNull
 import renetik.android.core.kotlin.text.StringBuilder
 import renetik.android.core.kotlin.text.add
 import renetik.android.core.kotlin.text.deleteLast
 import renetik.android.core.kotlin.text.reload
+import renetik.android.core.lang.catchWarnReturn
+import renetik.android.core.lang.catchWarnReturnNull
 import java.nio.charset.StandardCharsets
 import java.text.Normalizer
 import java.util.*
@@ -18,30 +18,30 @@ val String.Companion.Empty get() = ""
 val String.Companion.Space get() = " "
 
 fun String.Companion.random(length: Int): String {
-    val random = Random()
-    val letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    val text = CharArray(length)
-    for (i in 0 until length)
-        text[i] = letters[random.nextInt(letters.length)]
-    return String(text)
+	val random = Random()
+	val letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	val text = CharArray(length)
+	for (i in 0 until length)
+		text[i] = letters[random.nextInt(letters.length)]
+	return String(text)
 }
 
 fun String.Companion.contains(string1: String?, string2: String?, ignoreCase: Boolean = false) =
-    if (string1 == null || string2 == null) false
-    else string1.contains(string2, ignoreCase)
+	if (string1 == null || string2 == null) false
+	else string1.contains(string2, ignoreCase)
 
 fun String.Companion.range(start: Int, endInclusive: Int): List<String> =
-    IntRange(start, endInclusive).map { "$it" }
+	IntRange(start, endInclusive).map { "$it" }
 
 val String?.isEmpty get() = this?.trim()?.isEmpty() ?: true
 val String?.isSet get() = !this.isEmpty
 val String?.setOrNull get() = if (isSet) this else null
 fun String.ifEmpty(function: (String) -> Unit) = apply {
-    if (this.isEmpty) function(this)
+	if (this.isEmpty) function(this)
 }
 
 fun String.ifSet(function: (String) -> Unit) = apply {
-    if (this.isSet) function(this)
+	if (this.isSet) function(this)
 }
 
 
@@ -50,40 +50,40 @@ fun String.asFloat() = catchWarnReturnNull<Float, NumberFormatException> { toFlo
 fun String.asDouble() = catchWarnReturnNull<Double, NumberFormatException> { toDouble() }
 fun String.asInt() = catchWarnReturnNull<Int, NumberFormatException> { toInt() }
 fun String.asDouble(default: Double) =
-    catchWarnReturn<Double, NumberFormatException>(default) { toDouble() }
+	catchWarnReturn<Double, NumberFormatException>(default) { toDouble() }
 
 fun String.asLong(default: Long) =
-    catchWarnReturn<Long, NumberFormatException>(default) { toLong() }
+	catchWarnReturn<Long, NumberFormatException>(default) { toLong() }
 
 fun String.asFloat(default: Float) =
-    catchWarnReturn<Float, NumberFormatException>(default) { toFloat() }
+	catchWarnReturn<Float, NumberFormatException>(default) { toFloat() }
 
 fun String.asInt(default: Int) =
-    catchWarnReturn<Int, NumberFormatException>(default) { toInt() }
+	catchWarnReturn<Int, NumberFormatException>(default) { toInt() }
 
 fun String.trimNewLines() = replace(String.NewLine, String.Empty)
 
 fun String.remove(toRemove: String) = replace(toRemove, "")
 
 fun String.replace(strings: Array<String>, replacement: String): String {
-    val builder = StringBuilder(this)
-    strings.forEach { builder.reload(builder.toString().replace(it, replacement)) }
-    return builder.toString()
+	val builder = StringBuilder(this)
+	strings.forEach { builder.reload(builder.toString().replace(it, replacement)) }
+	return builder.toString()
 }
 
 fun String.leaveEndOfLength(length: Int): String {
-    if (this.length > length) {
-        val toCut = this.length - length
-        return substring(toCut)
-    }
-    return this
+	if (this.length > length) {
+		val toCut = this.length - length
+		return substring(toCut)
+	}
+	return this
 }
 
 fun String.separateToString(vararg items: Any?): String {
-    val text = StringBuilder()
-    for (value in items) value.notNull { text.add(it).add(this) }
-    if (!text.isEmpty) text.deleteLast(this.length)
-    return text.toString()
+	val text = StringBuilder()
+	for (value in items) value.notNull { text.add(it).add(this) }
+	if (!text.isEmpty) text.deleteLast(this.length)
+	return text.toString()
 }
 
 val String.lowerCased: String get() = lowercase(Locale.ROOT)
@@ -95,22 +95,22 @@ private val nonSpacingCharactersRegex = "\\p{Mn}+".toRegex()
 // private val combiningdiacriticalMarksRegex = "\\p{Mn}+".toRegex()
 
 fun String.removeAccents(): String {
-    return Normalizer.normalize(this, Normalizer.Form.NFD)
-        .replace(nonSpacingCharactersRegex, "")
+	return Normalizer.normalize(this, Normalizer.Form.NFD)
+		.replace(nonSpacingCharactersRegex, "")
 }
 
 fun String.toMaxBytesSize(length: Int): String {
-    val nameBytes = toByteArray(StandardCharsets.UTF_8)
-    return if (nameBytes.size > length) {
-        val bytes = ByteArray(length)
-        System.arraycopy(nameBytes, 0, bytes, 0, length)
-        String(bytes, StandardCharsets.UTF_8)
-    } else this
+	val nameBytes = toByteArray(StandardCharsets.UTF_8)
+	return if (nameBytes.size > length) {
+		val bytes = ByteArray(length)
+		System.arraycopy(nameBytes, 0, bytes, 0, length)
+		String(bytes, StandardCharsets.UTF_8)
+	} else this
 }
 
 fun String.maxLengthOf(count: Int) = if (count > count) substring(0 until 10) else this
 
 fun String.containsAll(words: List<String>, ignoreCase: Boolean = false): Boolean {
-    for (word in words) if (!contains(word, ignoreCase)) return false
-    return true
+	for (word in words) if (!contains(word, ignoreCase)) return false
+	return true
 }
