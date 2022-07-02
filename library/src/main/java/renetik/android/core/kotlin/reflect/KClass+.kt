5a211656a0e3f6771185a53ab0b8aefca3c6f167
Rene Dohan <dohan.rene@gmail.com>
2022-07-02 14:08:10 -0500

    Update KClass+.kt

--- library/src/main/java/renetik/android/core/kotlin/reflect/KClass+.kt
+++ library/src/main/java/renetik/android/core/kotlin/reflect/KClass+.kt
@@ -2,6 +2,7 @@ package renetik.android.core.kotlin.reflect
 
 import renetik.android.core.lang.catchAllWarnReturnNull
 import kotlin.reflect.KClass
+import kotlin.reflect.full.createInstance
 
 fun <T : Any> KClass<T>.createInstance() = catchAllWarnReturnNull {
     java.getDeclaredConstructor().run {
@@ -9,3 +10,5 @@ fun <T : Any> KClass<T>.createInstance() = catchAllWarnReturnNull {
         newInstance()
     }
 }
+
+inline fun <reified T : Any> createInstance(): T = T::class.createInstance()
