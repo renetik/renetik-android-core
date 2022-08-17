8d8db4a2662ab578225124684c03016129aa1a7e
Rene Dohan <dohan.rene@gmail.com>
2022-08-18 01:05:34 +0200

    ...

---
+++ library/src/main/java/renetik/android/core/math/CSPoint+.kt
@@ -0,0 +1,4 @@
+package renetik.android.core.math
+
+val <T : Number> CSPoint<T>.left get() = x
+val <T : Number> CSPoint<T>.top get() = y
--- library/src/main/java/renetik/android/core/math/CSPoint.kt
+++ library/src/main/java/renetik/android/core/math/CSPoint.kt
@@ -1,3 +1,4 @@
 package renetik.android.core.math
 
 data class CSPoint<T : Number>(val x: T, val y: T)
+
