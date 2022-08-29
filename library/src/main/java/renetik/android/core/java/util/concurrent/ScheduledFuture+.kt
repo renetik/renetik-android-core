7d3d1444f3384d5af639de9b2e7ef3f76675a3d2
Rene Dohan <dohan.rene@gmail.com>
2022-08-30 00:00:49 +0200

    Update ScheduledFuture+.kt

--- library/src/main/java/renetik/android/core/java/util/concurrent/ScheduledFuture+.kt
+++ library/src/main/java/renetik/android/core/java/util/concurrent/ScheduledFuture+.kt
@@ -3,4 +3,4 @@ package renetik.android.core.java.util.concurrent
 import java.util.concurrent.ScheduledFuture
 
 fun ScheduledFuture<*>.cancel() = cancel(false)
-fun ScheduledFuture<*>.cancel(interrupt: Boolean = false) = cancel(interrupt)
+fun ScheduledFuture<*>.cancel(interrupt: Boolean) = cancel(interrupt)
