18d76d0677ee3a6783d6482ce8e42d77bb2958c5
Rene Dohan <dohan.rene@gmail.com>
2022-09-18 00:17:24 +0200

    Update ThreadPoolExecutor+.kt

--- library/src/main/java/renetik/android/core/java/util/concurrent/ThreadPoolExecutor+.kt
+++ library/src/main/java/renetik/android/core/java/util/concurrent/ThreadPoolExecutor+.kt
@@ -1,13 +1,10 @@
 package renetik.android.core.java.util.concurrent
 
-import renetik.android.core.logging.CSLog
-import renetik.android.core.logging.CSLogMessage
 import java.util.concurrent.ThreadPoolExecutor
 
-fun ThreadPoolExecutor.logExecutorInfo() = CSLog.logInfo {
-    CSLogMessage.message("completedTaskCount:${completedTaskCount}\n" +
+val ThreadPoolExecutor.debugString
+    get() = "completedTaskCount:${completedTaskCount}\n" +
             "taskCount:${taskCount}\n" +
             "corePoolSize:${corePoolSize}\n" +
             "poolSize:${poolSize}\n" +
-            "activeCount:${activeCount}")
-}
+            "activeCount:${activeCount}"
