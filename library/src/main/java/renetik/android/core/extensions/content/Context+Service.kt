073a258fa15ee346af445258811de31c75f7b940
Rene Dohan <dohan.rene@gmail.com>
2022-09-22 19:07:37 +0200

    refa + player startTime after bpm change fix

--- library/src/main/java/renetik/android/core/extensions/content/Context+Service.kt
+++ library/src/main/java/renetik/android/core/extensions/content/Context+Service.kt
@@ -1,5 +1,6 @@
 package renetik.android.core.extensions.content
 
+import android.annotation.SuppressLint
 import android.app.Service
 import android.bluetooth.BluetoothManager
 import android.content.Context
@@ -8,7 +9,12 @@ import android.content.ContextWrapper.MIDI_SERVICE
 import android.content.Intent
 import android.media.AudioManager
 import android.media.midi.MidiManager
+import android.os.PowerManager
 import android.view.inputmethod.InputMethodManager
+import renetik.android.core.kotlin.className
+import renetik.android.core.logging.CSLog.logError
+import renetik.android.core.logging.CSLog.logInfo
+import renetik.android.core.logging.CSLogMessage.Companion.message
 
 val Context.audioService get() = getSystemService(AUDIO_SERVICE) as AudioManager
 val Context.bluetoothService get() = getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
@@ -17,6 +23,16 @@ val Context.inputService get() = getSystemService(INPUT_METHOD_SERVICE) as Input
 val Context.midiService: MidiManager? get() = getSystemService(MIDI_SERVICE) as? MidiManager
 val Context.isMidiSupported: Boolean get() = midiService != null
 fun <T> Context.ifHasMidi(function: (MidiManager) -> T): T? = midiService?.let { function(it) }
+
+@SuppressLint("WakelockTimeout")
+fun Context.wakeLock(levelAndFlags: Int): PowerManager.WakeLock {
+    val lock = (getSystemService(POWER_SERVICE) as PowerManager)
+        .newWakeLock(levelAndFlags, "wakeLock:${className}")
+    lock.acquire()
+    if (lock.isHeld) logInfo { message("Wake Lock held: $this") }
+    else logError { message("Wake Lock not held: $this") }
+    return lock
+}
 
 fun Context.startService(serviceClass: Class<out Service>) =
     startService(Intent(this, serviceClass))
