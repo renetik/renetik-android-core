50b1cfed3fb995cddf46c1c18da83247edd504fa
Rene Dohan - Renetik <dohan.rene@gmail.com>
2022-11-20 23:50:18 +0400

    ...

--- library/src/main/java/renetik/android/core/lang/CSTypealias.kt
+++ library/src/main/java/renetik/android/core/lang/CSTypealias.kt
@@ -1,6 +1,7 @@
 package renetik.android.core.lang
 
 typealias void = Unit
+typealias Void = Unit
 typealias Func = () -> void
 typealias ArgFunc<Arg> = (Arg) -> void
 typealias ArgArgFunc<Arg1, Arg2> = (Arg1, Arg2) -> void
