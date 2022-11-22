e2083cc75878b5784c6bb19e428441cc063b50f9
Rene Dohan - Renetik <dohan.rene@gmail.com>
2022-11-22 22:48:21 +0400

    ...

--- library/src/main/java/renetik/android/core/lang/variable/CSVariable+.kt
+++ library/src/main/java/renetik/android/core/lang/variable/CSVariable+.kt
@@ -7,27 +7,28 @@ fun <T> CSVariable<T>.value(value: T) = apply { this.value = value }
 fun CSVariable<Boolean>.setTrue() = apply { this.value = true }
 fun CSVariable<Boolean>.setFalse() = apply { this.value = false }
 fun CSVariable<Boolean>.toggle() = apply { value = !value }
-inline var CSVariable<Boolean>.isTrue
+
+inline var CSVariable<Boolean>.isTrue: Boolean
     get() = value
     set(newValue) {
         value = newValue
     }
 
-inline var CSVariable<Boolean>.isFalse
+inline var CSVariable<Boolean>.isFalse: Boolean
     get() = !value
     set(newValue) {
         value = !newValue
     }
 
 
-inline var CSVariable<String?>.string
+inline var CSVariable<String?>.string: String
     get() = value.asString
     set(newValue) {
         value = newValue
     }
 
-fun <T : CSVariable<Int>> T.increment() = apply { value++ }
-fun <T : CSVariable<Int>> T.decrement() = apply { value-- }
+fun <T : CSVariable<Int>> T.increment(): T = apply { value++ }
+fun <T : CSVariable<Int>> T.decrement(): T = apply { value-- }
 
 fun CSVariable<Double>.value(value: Int) = apply { this.value = value.toDouble() }
 fun CSVariable<Float>.value(value: Number) = apply { this.value = value.toFloat() }
