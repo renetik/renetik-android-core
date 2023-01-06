a14470156b3c5cf1b2c15ff2035db1f6a5ac1190
Rene Dohan - Renetik <dohan.rene@gmail.com>
2023-01-06 11:54:01 +0400

    ...

--- library/src/main/java/renetik/android/core/extensions/app/Application+.kt
+++ library/src/main/java/renetik/android/core/extensions/app/Application+.kt
@@ -6,7 +6,7 @@ import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.os.Process
 import kotlin.system.exitProcess
 
-fun Application.restart() {
+fun Application.hardRestart() {
     val intent = packageManager.getLaunchIntentForPackage(packageName)!!
     intent.flags = FLAG_ACTIVITY_NEW_TASK or FLAG_ACTIVITY_CLEAR_TASK
     startActivity(intent)
