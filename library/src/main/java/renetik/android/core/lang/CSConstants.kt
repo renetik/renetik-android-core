40695349573b5d62f99d4177629c0448d8245e3b
Rene Dohan <dohan.rene@gmail.com>
2023-01-23 02:42:48 +0400

    ...

--- library/src/main/java/renetik/android/core/lang/CSConstants.kt
+++ library/src/main/java/renetik/android/core/lang/CSConstants.kt
@@ -8,7 +8,7 @@ object CSLang {
 
 object CSStringConstants {
     const val Empty = ""
-    const val Ellipsize = "..."
+    const val Ellipsize = "…"
     const val Comma = ","
     const val Semicolon = ";"
     const val NewLine = "\n"
