71e68c59f3f2a8134e05df1859d09861e78d3a02
Rene Dohan <dohan.rene@gmail.com>
2023-02-26 20:03:48 +0100

    ...

--- library/src/main/java/renetik/android/core/kotlin/Any+CSExtension.kt
+++ library/src/main/java/renetik/android/core/kotlin/Any+CSExtension.kt
@@ -11,6 +11,10 @@ inline fun <T : Any> T.then(function: (T) -> Unit): Unit = function(this)
 inline fun <T : Any> T.changeIf(condition: Boolean, function: (T) -> T) =
     if (condition) function(this) else this
 
+inline fun <T : Any> T.runIf(condition: Boolean, function: (T) -> Unit) = apply {
+    if (condition) function(this)
+}
+
 inline val <T : Any> T.className get() = this::class.simpleName
 
 fun Any?.toId() = (this as? CSHasId)?.id ?: toString()
