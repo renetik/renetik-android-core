c681da04c7da7981b52a6ff113bab79206954593
Rene Dohan <dohan.rene@gmail.com>
2023-03-12 19:52:59 +0100

    ...

--- library/src/main/java/renetik/android/core/lang/CSBackground.kt
+++ library/src/main/java/renetik/android/core/lang/CSBackground.kt
@@ -1,5 +1,6 @@
 package renetik.android.core.lang
 
+import android.os.HandlerThread
 import androidx.annotation.WorkerThread
 import renetik.android.core.java.util.concurrent.background
 import renetik.android.core.java.util.concurrent.backgroundNano
@@ -7,31 +8,44 @@ import renetik.android.core.java.util.concurrent.backgroundRepeat
 import java.util.concurrent.ScheduledFuture
 import java.util.concurrent.ScheduledThreadPoolExecutor
 
+
 object CSBackground {
     val executor = ScheduledThreadPoolExecutor(3)
 
+//    val handler by lazy {
+//        val handlerThread = HandlerThread("backgroundThread")
+//        if (!handlerThread.isAlive) handlerThread.start()
+//        handlerThread
+//    }
+
     fun background(
-        @WorkerThread function: Func): ScheduledFuture<*> =
+        @WorkerThread function: Func
+    ): ScheduledFuture<*> =
         executor.background(function = function)
 
     fun background(
-        delay: Long = 0, @WorkerThread function: () -> Unit): ScheduledFuture<*> =
+        delay: Long = 0, @WorkerThread function: () -> Unit
+    ): ScheduledFuture<*> =
         executor.background(delay = delay, function = function)
 
     fun backgroundNano(
-        delay: Long = 0, @WorkerThread function: () -> Unit): ScheduledFuture<*> =
+        delay: Long = 0, @WorkerThread function: () -> Unit
+    ): ScheduledFuture<*> =
         executor.backgroundNano(delay = delay, function = function)
 
     fun background(
-        delay: Int = 0, @WorkerThread function: () -> Unit): ScheduledFuture<*> =
+        delay: Int = 0, @WorkerThread function: () -> Unit
+    ): ScheduledFuture<*> =
         executor.background(delay = delay.toLong(), function = function)
 
     fun backgroundRepeat(
-        delay: Long, period: Long, @WorkerThread function: () -> Unit): ScheduledFuture<*> =
+        delay: Long, period: Long, @WorkerThread function: () -> Unit
+    ): ScheduledFuture<*> =
         executor.backgroundRepeat(delay = delay, period = period, function = function)
 
     fun backgroundRepeat(
-        period: Long, @WorkerThread function: () -> Unit): ScheduledFuture<*> =
+        period: Long, @WorkerThread function: () -> Unit
+    ): ScheduledFuture<*> =
         executor.backgroundRepeat(period, function)
 }
 
