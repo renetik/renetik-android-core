26f3e6ffd5af717951af5361c748d99562e06cd2
Rene Dohan <dohan.rene@gmail.com>
2023-03-20 19:07:06 +0100

    ...

--- library/src/test/java/renetik/android/core/lang/variable/CSWeakVariableTest.kt
+++ library/src/test/java/renetik/android/core/lang/variable/CSWeakVariableTest.kt
@@ -4,6 +4,7 @@ import org.junit.Assert.*
 import org.junit.Test
 import renetik.android.core.lang.variable.CSWeakVariable.Companion.weak
 
+@Suppress("UNUSED_VALUE")
 class CSWeakVariableTest {
 
     class TestObject
