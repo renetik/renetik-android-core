b0a69644b8ad2040eedb0797bdc3951388dc5ffe
Rene Dohan <dohan.rene@gmail.com>
2023-03-24 23:27:42 +0100

    ...

--- library/src/main/java/renetik/android/core/extensions/graphics/Rect+.kt
+++ library/src/main/java/renetik/android/core/extensions/graphics/Rect+.kt
@@ -1,7 +1,17 @@
 package renetik.android.core.extensions.graphics
 
 import android.graphics.Rect
+import renetik.android.core.math.CSPoint
+import kotlin.math.max
+import kotlin.math.min
 
 val Rect.width get() = width()
 val Rect.height get() = height()
 val Rect.debugString get() = "left:$left top:$top width:$width height:$height"
+fun Rect.load(start: CSPoint<Float>, end: CSPoint<Float>) = apply {
+    val left = min(start.x, end.x)
+    val top = min(start.y, end.y)
+    val right = max(start.x, end.x)
+    val bottom = max(start.y, end.y)
+    set(left.toInt(), top.toInt(), right.toInt(), bottom.toInt())
+}
