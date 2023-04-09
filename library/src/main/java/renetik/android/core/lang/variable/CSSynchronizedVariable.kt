1a4f513bee5490df11b766de1b9dffa277ac059d
Rene Dohan <dohan.rene@gmail.com>
2023-04-10 00:10:17 +0200

    ...

--- library/src/main/java/renetik/android/core/lang/variable/CSSynchronizedVariable.kt
+++ library/src/main/java/renetik/android/core/lang/variable/CSSynchronizedVariable.kt
@@ -1,7 +1,5 @@
 package renetik.android.core.lang.variable
 
 import renetik.android.core.lang.value.CSSynchronizedValue
-import renetik.android.core.lang.variable.CSVariable
 
 interface CSSynchronizedVariable<T> : CSSynchronizedValue<T>, CSVariable<T>
-
--- library/src/main/java/renetik/android/core/lang/variable/CSVariableImpl.kt
+++ library/src/main/java/renetik/android/core/lang/variable/CSVariableImpl.kt
@@ -1,18 +1,20 @@
 package renetik.android.core.lang.variable
 
 import renetik.android.core.lang.ArgFunc
+import kotlin.reflect.KProperty
 
 class CSVariableImpl<T>(
-    value: T,
-    val onChange: ArgFunc<T>? = null) : CSVariable<T> {
+    override var value: T,
+    val onChange: ArgFunc<T>? = null
+) : CSVariable<T> {
 
-    override var value: T = value
-        set(value) {
-            field = value
-            onChange?.invoke(value)
-        }
+    fun apply() = apply { onChange?.invoke(value) }
 
-    fun apply() = apply {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T =
+        synchronized(this) { value }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        synchronized(this) { this.value = value }
         onChange?.invoke(value)
     }
 }
