15608a42ed4d2080dc9e88b6888ec06bdc49f496
Rene Dohan <dohan.rene@gmail.com>
2023-05-06 13:13:33 +0200

    ...

--- library/src/main/java/renetik/android/core/extensions/content/Context+Activity.kt
+++ library/src/main/java/renetik/android/core/extensions/content/Context+Activity.kt
@@ -2,12 +2,14 @@ package renetik.android.core.extensions.content
 
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import androidx.appcompat.app.AppCompatActivity
 
 inline fun <reified Activity : AppCompatActivity>
         Context.startActivity(extras: Map<String, String> = emptyMap()) {
     val intent = Intent(this, Activity::class.java)
     for ((key, value) in extras) intent.putExtra(key, value)
-    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    intent.addFlags(FLAG_ACTIVITY_NEW_TASK or FLAG_ACTIVITY_CLEAR_TASK);
     startActivity(intent)
 }
