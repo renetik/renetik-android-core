55e09ad7e85c3b0608be977ea132fbd54cafa1de
Rene Dohan <dohan.rene@gmail.com>
2023-05-06 13:13:06 +0200

    ...

--- library/src/main/java/renetik/android/core/lang/CSLeakCanaryInterface.kt
+++ library/src/main/java/renetik/android/core/lang/CSLeakCanaryInterface.kt
@@ -2,7 +2,7 @@ package renetik.android.core.lang
 
 interface CSLeakCanaryInterface {
     fun Any.expectWeaklyReachable(description: String) = Unit
-    val enabled: Boolean get() = false
+    val isEnabled: Boolean get() = false
     fun enable() = Unit
     fun disable() = Unit
 }
