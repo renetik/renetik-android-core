0f9c458a5e99427d7f9e6022b556f3f0bebbb9e7
Rene Dohan <dohan.rene@gmail.com>
2023-05-12 23:24:47 +0200

    ...

--- library/src/main/java/renetik/android/core/java/lang/Class+CSExtension.kt
+++ library/src/main/java/renetik/android/core/java/lang/Class+CSExtension.kt
@@ -3,3 +3,12 @@ package renetik.android.core.java.lang
 import renetik.android.core.lang.catchAllWarnReturnNull
 
 fun <T> Class<T>.createInstance(): T? = catchAllWarnReturnNull { this.newInstance() }
+
+@Suppress("UNCHECKED_CAST")
+fun <T> Class<T>.createInstance(param: Any): T? = catchAllWarnReturnNull {
+    constructors.firstOrNull {
+        it.parameterTypes.size == 1
+    }?.also {
+        it.isAccessible = true
+    }?.newInstance(param) as? T
+}
--- library/src/main/java/renetik/android/core/kotlin/reflect/KClass+.kt
+++ library/src/main/java/renetik/android/core/kotlin/reflect/KClass+.kt
@@ -3,12 +3,14 @@ package renetik.android.core.kotlin.reflect
 import renetik.android.core.lang.catchAllWarnReturnNull
 import kotlin.reflect.KClass
 import kotlin.reflect.full.createInstance
+import kotlin.reflect.full.primaryConstructor
 
 fun <T : Any> KClass<T>.createInstance() = catchAllWarnReturnNull {
-    java.getDeclaredConstructor().run {
-        isAccessible = true
-        newInstance()
-    }
+    createInstance()
+}
+
+fun <T : Any> KClass<T>.createInstance(param: Any) = catchAllWarnReturnNull {
+    primaryConstructor?.call(param)
 }
 
 inline fun <reified T : Any> createInstance(): T = T::class.createInstance()
