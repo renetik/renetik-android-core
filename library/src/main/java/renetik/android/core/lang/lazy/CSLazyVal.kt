ac7e6c4f4862e23d4e85d058b07e0480bc9efdca
Rene Dohan <dohan.rene@gmail.com>
2023-06-25 13:57:59 +0200

    ...

--- library/src/main/java/renetik/android/core/lang/lazy/CSLazyVal.kt
+++ library/src/main/java/renetik/android/core/lang/lazy/CSLazyVal.kt
@@ -3,7 +3,31 @@ package renetik.android.core.lang.lazy
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
-class CSLazyVal<T>(private val onLoad: () -> T) : ReadOnlyProperty<Any?, T> {
+//class CSLazyVal<T>(private val onLoad: () -> T) : ReadOnlyProperty<Any?, T> {
+//    private var isInitialized = false
+//    private var value: T? = null
+//
+//    fun reset() {
+//        value = null
+//        isInitialized = false
+//    }
+//
+//    override fun getValue(
+//        thisRef: Any?, property: KProperty<*>
+//    ): T = synchronized(this) {
+//        if (!isInitialized) {
+//            value = onLoad()
+//            isInitialized = true
+//        }
+//        return value!!
+//    }
+//
+//    companion object {
+//        fun <T> lazyVal(initializer: () -> T) = CSLazyVal(initializer)
+//    }
+//}
+
+class CSLazyVal<T>(private val onLoad: () -> T) : ReadOnlyProperty<Any?, T?> {
     private var isInitialized = false
     private var value: T? = null
 
@@ -12,14 +36,12 @@ class CSLazyVal<T>(private val onLoad: () -> T) : ReadOnlyProperty<Any?, T> {
         isInitialized = false
     }
 
-    override fun getValue(
-        thisRef: Any?, property: KProperty<*>
-    ): T = synchronized(this) {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T? = synchronized(this) {
         if (!isInitialized) {
             value = onLoad()
             isInitialized = true
         }
-        return value!!
+        return value
     }
 
     companion object {
