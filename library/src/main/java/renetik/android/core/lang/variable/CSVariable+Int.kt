32a5fe44d2298d58abac51e24471f7225b2e521f
Rene Dohan <dohan.rene@gmail.com>
2023-07-11 12:14:00 +0200

    ...

--- library/src/main/java/renetik/android/core/lang/variable/CSVariable+Int.kt
+++ library/src/main/java/renetik/android/core/lang/variable/CSVariable+Int.kt
@@ -4,10 +4,10 @@ fun <T : CSVariable<Int>> T.increment(): T = apply { value++ }
 
 fun <T : CSVariable<Int>> T.decrement(): T = apply { value-- }
 
-operator fun CSVariable<Int>.minusAssign(step: Int) {
-    value -= step
+operator fun CSVariable<Int>.minusAssign(value: Int) {
+    this.value -= value
 }
 
-operator fun CSVariable<Int>.plusAssign(step: Int) {
-    value += step
+operator fun CSVariable<Int>.plusAssign(value: Int) {
+    this.value += value
 }
