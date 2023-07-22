9993811d15830d9c46082dbbc9bce617bbba6c15
Rene Dohan <dohan.rene@gmail.com>
2023-07-22 15:09:49 +0200

    ...

--- library/src/main/java/renetik/android/core/lang/CSLayoutRes.kt
+++ library/src/main/java/renetik/android/core/lang/CSLayoutRes.kt
@@ -5,5 +5,9 @@ import androidx.annotation.LayoutRes
 data class CSLayoutRes(@LayoutRes val id: Int) {
     companion object {
         fun layout(@LayoutRes id: Int) = CSLayoutRes(id)
+
+        //@get:LayoutRes // TODO: make this check work
+        val Int.layout get() = CSLayoutRes(this)
     }
 }
+
