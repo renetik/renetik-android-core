214b3a94fbea469e8ad9448c0c76ef557a69cffc
Rene Dohan <dohan.rene@gmail.com>
2023-07-30 17:25:49 +0200

    ...

--- library/src/main/java/renetik/android/core/lang/atomic/CSAtomic.kt
+++ library/src/main/java/renetik/android/core/lang/atomic/CSAtomic.kt
@@ -4,14 +4,8 @@ import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 class CSAtomic<T>(@Volatile private var value: T?) : ReadWriteProperty<Any?, T?> {
-
-    override fun getValue(
-        thisRef: Any?, property: KProperty<*>
-    ): T? = value
-
-    override fun setValue(
-        thisRef: Any?, property: KProperty<*>, value: T?
-    ) {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T? = value
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T?) {
         this.value = value
     }
 
