86ac4165d8ef2c517e5347497ed8928b13368a8c
Rene Dohan <dohan.rene@gmail.com>
2023-08-03 23:13:16 +0200

    ...

--- library/src/main/java/renetik/android/core/lang/CSLang.kt
+++ library/src/main/java/renetik/android/core/lang/CSLang.kt
@@ -6,3 +6,5 @@ object CSLang {
 
 inline fun <T : Any, R> synchronized(lock: T, block: (T) -> R): R =
     kotlin.synchronized(lock) { block(lock) }
+
+inline fun invoke(block: () -> Unit): Unit = block()
