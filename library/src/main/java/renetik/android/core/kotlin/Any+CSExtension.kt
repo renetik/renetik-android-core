b4376cff000424cfcb4117f6185939f1dd90fcfe
Rene Dohan <dohan.rene@gmail.com>
2023-10-17 17:34:52 +0800

    Remove onNotNull

--- library/src/main/java/renetik/android/core/kotlin/Any+CSExtension.kt
+++ library/src/main/java/renetik/android/core/kotlin/Any+CSExtension.kt
@@ -2,18 +2,16 @@ package renetik.android.core.kotlin
 
 import renetik.android.core.lang.CSHasId
 
-inline fun <T, R> onNotNull(receiver: T?, block: T.() -> R) {
-    receiver?.let { receiver.block() }
-}
-
 inline fun <T : Any> T.then(function: (T) -> Unit): Unit = function(this)
 
 inline fun <T : Any> T.changeIf(condition: Boolean, function: (T) -> T) =
     if (condition) function(this) else this
 
-inline fun <T : Any> T.alsoIf(condition: Boolean, function: (T) -> Unit) = also {
-    if (condition) function(it)
-}
+inline fun <T : Any> T.alsoIf(condition: Boolean, function: (T) -> Unit) =
+    also { if (condition) function(it) }
+
+inline fun <T : Any> T.applyIf(condition: Boolean, function: (T).() -> Unit) =
+    apply { if (condition) function(this) }
 
 inline val <T : Any> T.className get() = this::class.simpleName
 
