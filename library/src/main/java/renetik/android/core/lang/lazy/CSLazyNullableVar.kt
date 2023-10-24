80913ff4d531647959f2e2f9261598dc5c322c17
Rene Dohan <dohan.rene@gmail.com>
2023-10-24 18:30:24 +0800

    ...

--- library/src/main/java/renetik/android/core/lang/lazy/CSLazyNullableVar.kt
+++ library/src/main/java/renetik/android/core/lang/lazy/CSLazyNullableVar.kt
@@ -4,24 +4,22 @@ import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 class CSLazyNullableVar<T>(initializer: () -> T) : ReadWriteProperty<Any?, T> {
-    private object initialValue
+    private object InitialValue
 
     private var isSet = false
     private val lazyValue by lazy(initializer)
-    private var value: Any? = initialValue
+    private var value: Any? = InitialValue
 
-    override fun getValue(thisRef: Any?, property: KProperty<*>): T =
-        synchronized(this) {
-            if (!isSet) return lazyValue
-            @Suppress("UNCHECKED_CAST")
-            return value as T
-        }
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T = synchronized(this) {
+        if (!isSet) return lazyValue
+        @Suppress("UNCHECKED_CAST")
+        return value as T
+    }
 
-    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) =
-        synchronized(this) {
-            this.value = value
-            isSet = true
-        }
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) = synchronized(this) {
+        this.value = value
+        isSet = true
+    }
 
     companion object {
         fun <T> lazyNullableVar(initializer: () -> T) = CSLazyNullableVar(initializer)
