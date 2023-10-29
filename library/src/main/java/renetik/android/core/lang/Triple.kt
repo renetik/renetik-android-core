2fff18204297489de009ebcaeb9b1581041c5ee0
Rene Dohan <dohan.rene@gmail.com>
2023-10-30 00:40:21 +0800

    add triple test.

--- library/src/main/java/renetik/android/core/lang/Triple.kt
+++ library/src/main/java/renetik/android/core/lang/Triple.kt
@@ -1,8 +1,3 @@
 package renetik.android.core.lang
 
 infix fun <A, B, C> Pair<A, B>.to(that: C): Triple<A, B, C> = Triple(first, second, that)
-
-fun test() {
-    val something = 45 to "Apple" to 57f
-    val (number: Int, string: String, float: Float) = something
-}
