1b819cc4cb73d762d111732e429a79f25febf772
Rene Dohan <dohan.rene@gmail.com>
2023-10-29 18:30:37 +0800

    ...

--- library/src/main/java/renetik/android/core/kotlin/collections/MutableCollection+.kt
+++ library/src/main/java/renetik/android/core/kotlin/collections/MutableCollection+.kt
@@ -2,15 +2,3 @@ package renetik.android.core.kotlin.collections
 
 fun <T> MutableCollection<T>.removeFirst(condition: (T) -> Boolean) =
     iterator().removeFirst(condition)
-
-fun <T> MutableIterator<T>.removeFirst(condition: (T) -> Boolean): Boolean {
-    for (item in this) if (condition.invoke(item)) {
-        remove()
-        return true
-    }
-    return false
-}
-
-fun <T> MutableIterator<T>.removeAll(condition: (T) -> Boolean) {
-    for (item in this) if (condition.invoke(item)) remove()
-}
