26e270e2d1cbb2cd765c03da652257179ddaebec
Rene Dohan <dohan.rene@gmail.com>
2023-11-27 21:22:09 +0800

    i18n

--- library/src/main/java/renetik/android/core/extensions/content/Context+Toast.kt
+++ library/src/main/java/renetik/android/core/extensions/content/Context+Toast.kt
@@ -1,13 +1,17 @@
 package renetik.android.core.extensions.content
 
-import android.content.Context
-import android.widget.Toast.*
 //import renetik.android.core.lang.CSEnvironment.app
+import android.content.Context
+import android.widget.Toast.LENGTH_LONG
+import android.widget.Toast.LENGTH_SHORT
+import android.widget.Toast.makeText
+import androidx.annotation.StringRes
 import renetik.android.core.extensions.content.CSToastLength.LongTime
 import renetik.android.core.extensions.content.CSToastLength.ShortTime
 import renetik.android.core.lang.CSEnvironment.app
 
 object CSToast {
+    fun toast(@StringRes text: Int) = app.toast(app.getString(text))
     fun toast(text: String) = app.toast(text)
     fun toast(text: String, time: CSToastLength = ShortTime) = app.toast(text, time)
 }
