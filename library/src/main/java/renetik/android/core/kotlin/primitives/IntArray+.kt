ab657244c27944f5f3cb19009d1cca75d5022527
Rene Dohan <dohan.rene@gmail.com>
2024-03-04 01:16:38 +0800

    ...

--- library/src/main/java/renetik/android/core/kotlin/primitives/IntArray+.kt
+++ library/src/main/java/renetik/android/core/kotlin/primitives/IntArray+.kt
@@ -4,7 +4,9 @@ import java.lang.System.arraycopy
 
 fun IntArray.rotate(offset: Int, start: Int = 0, end: Int = size) {
     require(start < end) { "startIndex must be less than endIndex" }
-    require(offset in indices) { "Invalid offsetIndex" }
+    require(offset in indices) {
+        "Invalid offsetIndex: $offset not in $indices"
+    }
     val array1 = sliceArray(start until offset)
     val array2 = sliceArray(offset until end)
     arraycopy(array2, 0, this, start, array2.size)
