a15a1605e1fef1dea5ce0a594077b7807255622f
Rene Dohan <dohan.rene@gmail.com>
2024-03-04 01:06:53 +0800

    ...

--- library/src/main/java/renetik/android/core/lang/Quadruple.kt
+++ library/src/main/java/renetik/android/core/lang/Quadruple.kt
@@ -13,3 +13,16 @@ data class Quadruple<out A, out B, out C, out D>(
 
 infix fun <A, B, C, D> Triple<A, B, C>.to(that: D): Quadruple<A, B, C, D> =
     Quadruple(first, second, third, that)
+
+data class Quintuple<out A, out B, out C, out D, out E>(
+    val first: A,
+    val second: B,
+    val third: C,
+    val fourth: D,
+    val fifth: E
+) : Serializable {
+    override fun toString(): String = "($first, $second, $third, $fourth, $fifth)"
+}
+
+infix fun <A, B, C, D, E> Quadruple<A, B, C, D>.to(that: E): Quintuple<A, B, C, D, E> =
+    Quintuple(first, second, third, fourth, that)
