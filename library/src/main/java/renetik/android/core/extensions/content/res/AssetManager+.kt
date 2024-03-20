c7f0f42164d2d9f33356a4624794404dafe53f04
Rene Dohan <dohan.rene@gmail.com>
2024-03-20 17:34:41 +0800

    Add mooji song

--- library/src/main/java/renetik/android/core/extensions/content/res/AssetManager+.kt
+++ library/src/main/java/renetik/android/core/extensions/content/res/AssetManager+.kt
@@ -1,9 +1,9 @@
 package renetik.android.core.extensions.content.res
 
 import android.content.res.AssetManager
-import renetik.android.core.kotlin.primitives.iterator
 import renetik.android.core.java.io.copy
 import renetik.android.core.java.io.createFileAndDirs
+import renetik.android.core.kotlin.primitives.iterator
 import java.io.File
 
 fun AssetManager.copyFilesToDir(path: String, targetDir: File, overwrite: Boolean = false) {
@@ -15,6 +15,32 @@ fun AssetManager.copyFilesToDir(path: String, targetDir: File, overwrite: Boolea
             }
     }
 }
+
+fun AssetManager.copyFilesToDir2(path: String, targetDir: File, overwrite: Boolean = false) {
+    val items = list(path)
+    for (item in items ?: emptyArray()) {
+        val assetPath = "$path/$item"
+        val targetPath = File(targetDir, item)
+        if (item.contains('.')) {
+            if (overwrite || !targetPath.exists()) {
+                targetPath.createFileAndDirs().outputStream().use {
+                    open(assetPath.replace("//", "/")).copy(it)
+                }
+            }
+        } else {
+            val subDir = File(targetDir, item)
+            if (!subDir.exists()) subDir.mkdirs()
+            copyFilesToDir2(assetPath, subDir, overwrite)
+        }
+    }
+}
+
+fun AssetManager.isFile(path: String): Boolean = !isDir(path)
+
+fun AssetManager.isDir(path: String): Boolean = !path.contains('.') //sadly...
+//    runCatching {
+//    openFd(path).let { it.length == AssetFileDescriptor.UNKNOWN_LENGTH && it.declaredLength < 0 }
+//}.getOrNull() ?: false
 
 fun AssetManager.copyFileToDir(assetPath: String, folder: File, overwrite: Boolean = false) {
     val target = File(folder, File(assetPath).name)
