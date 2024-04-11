7cd05449f0c0f3dd7bd8104e4852bd5a36dcc21a
Rene Dohan <dohan.rene@email.com>
2024-04-11 15:28:27 +0800

    Convert wave to floats.

--- library/src/main/java/renetik/android/core/kotlin/primitives/IntArray+.kt
+++ library/src/main/java/renetik/android/core/kotlin/primitives/IntArray+.kt
@@ -12,3 +12,14 @@ fun IntArray.rotate(offset: Int, start: Int = 0, end: Int = size) {
     arraycopy(array2, 0, this, start, array2.size)
     arraycopy(array1, 0, this, start + array2.size, array1.size)
 }
+
+fun FloatArray.rotate(offset: Int, start: Int = 0, end: Int = size) {
+    require(start < end) { "startIndex must be less than endIndex" }
+    require(offset in indices) {
+        "Invalid offsetIndex: $offset not in $indices"
+    }
+    val array1 = sliceArray(start until offset)
+    val array2 = sliceArray(offset until end)
+    arraycopy(array2, 0, this, start, array2.size)
+    arraycopy(array1, 0, this, start + array2.size, array1.size)
+}
