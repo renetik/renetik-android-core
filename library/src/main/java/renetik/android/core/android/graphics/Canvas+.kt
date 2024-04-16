cf141ae81237d1632bd219937823807ca9e430ee
Rene Dohan <dohan.rene@email.com>
2024-04-16 14:30:07 +0800

    ...

--- library/src/main/java/renetik/android/core/android/graphics/Canvas+.kt
+++ library/src/main/java/renetik/android/core/android/graphics/Canvas+.kt
@@ -5,3 +5,6 @@ import android.graphics.Paint
 
 fun Canvas.line(startX: Float, startY: Float, stopX: Float, stopY: Float, paint: Paint) =
     drawLine(startX, startY, stopX, stopY, paint)
+
+fun Canvas.rect(left: Float, top: Float, right: Float, bottom: Float, paint: Paint) =
+    drawRect(left, top, right, bottom, paint)
