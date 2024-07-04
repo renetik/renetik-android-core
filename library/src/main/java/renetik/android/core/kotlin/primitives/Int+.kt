461481fbf1aeed4a729daa9be33410717fe6fb8b
Renetik <dohan.rene@gmail.com>
2024-07-04 19:09:43 +0800

    ...

--- library/src/main/java/renetik/android/core/kotlin/primitives/Int+.kt
+++ library/src/main/java/renetik/android/core/kotlin/primitives/Int+.kt
@@ -14,9 +14,6 @@ import java.util.concurrent.atomic.AtomicInteger
 import kotlin.math.absoluteValue
 import kotlin.random.Random
 
-//TODO: Rename to uninitialized or remove ?
-inline val Int.Companion.Empty get() = MAX_VALUE
-
 private val counter = AtomicInteger(0)
 fun Int.Companion.unique(length: Int = 9): Int =
     "${counter.incrementAndGet()}${nanoTime()}".substring(0, length).toInt()
@@ -26,10 +23,6 @@ fun Int.Companion.random(min: Int = 0, max: Int = MAX_VALUE): Int {
     return Random.nextInt(max - min + 1) + min
 }
 
-//TODO: Remove es confusing
-inline val Int.isEmpty get() = this == Int.Empty
-//TODO: Remove es confusing
-inline val Int.isSet get() = !this.isEmpty
 fun Int.max(maximumValue: Int) = if (this < maximumValue) this else maximumValue
 fun Int.min(minimumValue: Int) = if (this > minimumValue) this else minimumValue
 
--- library/src/main/java/renetik/android/core/logging/CSLogMessage.kt
+++ library/src/main/java/renetik/android/core/logging/CSLogMessage.kt
@@ -5,7 +5,9 @@ import renetik.android.core.kotlin.primitives.Empty
 class CSLogMessage(val throwable: Throwable? = null, val message: String = "") {
     companion object {
         val Empty = CSLogMessage(null, String.Empty)
-        fun traceMessage(message: Any? = String.Empty) = CSLogMessage(Throwable(), message.toString())
+        fun traceMessage(message: Any? = String.Empty) =
+            CSLogMessage(Throwable(), message.toString())
+
         fun message(message: Any? = String.Empty) = CSLogMessage(null, message.toString())
         fun message(throwable: Throwable) = CSLogMessage(throwable = throwable)
         fun message(throwable: Throwable?, message: String? = String.Empty) =
