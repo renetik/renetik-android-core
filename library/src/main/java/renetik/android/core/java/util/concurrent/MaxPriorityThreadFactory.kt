aaa95635ce67174c1526920a86cc47d2d210ade9
Renetik <dohan.rene@gmail.com>
2024-07-08 23:19:59 +0800

    ...

--- library/src/main/java/renetik/android/core/java/util/concurrent/MaxPriorityThreadFactory.kt
+++ library/src/main/java/renetik/android/core/java/util/concurrent/MaxPriorityThreadFactory.kt
@@ -4,9 +4,7 @@ import java.lang.Thread.MAX_PRIORITY
 import java.util.concurrent.ThreadFactory
 
 class MaxPriorityThreadFactory : ThreadFactory {
-    override fun newThread(runnable: Runnable) = Thread {
-//        catchAllWarn { setThreadPriority(-20) }
-        runnable.run()
-    }.also { it.priority = MAX_PRIORITY }
+    override fun newThread(runnable: Runnable): Thread {
+        return Thread(runnable).apply { priority = MAX_PRIORITY }
+    }
 }
-
