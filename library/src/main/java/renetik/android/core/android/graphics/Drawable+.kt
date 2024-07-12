81041c166e1f522d2c21c82973432b7c239aa55d
Renetik <dohan.rene@gmail.com>
2024-07-12 16:35:12 +0800

    ...

--- library/src/main/java/renetik/android/core/android/graphics/Drawable+.kt
+++ library/src/main/java/renetik/android/core/android/graphics/Drawable+.kt
@@ -6,8 +6,13 @@ import android.graphics.Color.green
 import android.graphics.Color.red
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
 
 fun Drawable.toAlpha(alpha: Double): Drawable =
     (this as? ColorDrawable)?.color?.let {
         ColorDrawable(argb((256 * alpha).toInt(), red(it), green(it), blue(it)))
-    } ?: this.also { it.alpha = (255 * alpha).toInt() }
+    } ?: (this as? GradientDrawable)?.also {
+        it.alpha = (255 * alpha).toInt() // Not working it seems
+    } ?: run {
+        this.also { it.alpha = (255 * alpha).toInt() }
+    }
