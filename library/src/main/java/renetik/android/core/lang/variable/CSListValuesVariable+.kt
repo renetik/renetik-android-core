3f4524bfddeafc6fab32f82ade0e48ba44c6a8d2
Renetik <dohan.rene@gmail.com>
2024-07-27 12:15:00 +0800

    ...

--- library/src/main/java/renetik/android/core/lang/variable/CSListValuesVariable+.kt
+++ library/src/main/java/renetik/android/core/lang/variable/CSListValuesVariable+.kt
@@ -2,8 +2,8 @@ package renetik.android.core.lang.variable
 
 val <T : Enum<*>> CSListValuesVariable<T>.isLast get() = values.lastIndex == value.ordinal
 
-fun <T : Enum<*>> CSListValuesVariable<T>.next(): T = values[value.ordinal + 1]
+fun <T : Enum<*>> CSListValuesVariable<T>.getNext(): T = values[value.ordinal + 1]
 
-fun <T : Enum<*>> CSListValuesVariable<T>.previous(): T = values[value.ordinal - 1]
+fun <T : Enum<*>> CSListValuesVariable<T>.getPrevious(): T = values[value.ordinal - 1]
 
 fun <T> CSListValuesVariable<T>.value(index: Int) = value(values[index])
