aeaa00efe140f68191ec3eb7c89f40aaf5bf1b9f
Rene Dohan <dohan.rene@email.com>
2024-08-18 15:42:30 +0800

    ...

--- library/src/main/java/renetik/android/core/lang/value/CSValue+Int.kt
+++ library/src/main/java/renetik/android/core/lang/value/CSValue+Int.kt
@@ -1,5 +1,6 @@
 package renetik.android.core.lang.value
 
+operator fun Int.plus(value: CSValue<Int>): Int = this + value.value
+operator fun CSValue<Int>.plus(value: CSValue<Int>): Int = value.value + value.value
 operator fun CSValue<Int>.plus(value: Int): Int = this.value + value
-
 operator fun CSValue<Int>.compareTo(value: Int): Int = this.value.compareTo(value)
