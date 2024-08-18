392ad2cee9e356e2657f9926429efe23cc43bfb1
Rene Dohan <dohan.rene@email.com>
2024-08-18 13:39:37 +0800

    ...

--- library/src/main/java/renetik/android/core/lang/direction/CSVerticalDirection.kt
+++ library/src/main/java/renetik/android/core/lang/direction/CSVerticalDirection.kt
@@ -4,8 +4,8 @@ import renetik.android.core.R
 import renetik.android.core.base.CSApplication.Companion.getString
 
 enum class CSVerticalDirection(override val title: String) : CSDirection {
-    Bottom(getString(R.string.cs_direction_vertical_from_top)),
-    Top(getString(R.string.cs_direction_vertical_from_bottom));
+    Bottom(getString(R.string.cs_direction_vertical_bottom)),
+    Top(getString(R.string.cs_direction_vertical_top));
 
     override val isHorizontal: Boolean = false
 }
