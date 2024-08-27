0854596df657b519a0c10cd299b4de254acf5406
Rene Dohan <dohan.rene@email.com>
2024-08-27 21:04:09 +0800

    ...

--- library/src/main/java/renetik/android/core/kotlin/ranges/ClosedRange+.kt
+++ library/src/main/java/renetik/android/core/kotlin/ranges/ClosedRange+.kt
@@ -2,4 +2,6 @@ package renetik.android.core.kotlin.ranges
 
 val ClosedRange<Float>.size: Float get() = endInclusive - start
 val ClosedRange<Int>.size: Int get() = endInclusive - start
+val ClosedRange<Int>.from: Int get() = start
+val ClosedRange<Int>.to: Int get() = endInclusive
 val ClosedRange<Double>.size: Double get() = endInclusive - start
