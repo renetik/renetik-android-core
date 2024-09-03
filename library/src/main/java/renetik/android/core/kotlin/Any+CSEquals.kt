020b30be651e6ca1ccdab67f18049288a701df23
Rene Dohan <dohan.rene@email.com>
2024-09-03 13:07:53 +0800

    ....

--- library/src/main/java/renetik/android/core/kotlin/Any+CSEquals.kt
+++ library/src/main/java/renetik/android/core/kotlin/Any+CSEquals.kt
@@ -1,14 +1,28 @@
 package renetik.android.core.kotlin
 
-fun Any?.equalsAny(vararg items: Any?) = equalsAny(items.asIterable())
-infix fun Any?.equalsAny(items: Iterable<Any?>): Boolean {
-	for (item in items) if (this == item) return true; return false
+fun Any?.equalsAny(vararg items: Any?): Boolean {
+    for (item in items) if (this == item) return true
+    return false
 }
 
-fun Any?.equalsNone(vararg items: Any?) = equalsNone(items.asIterable())
+infix fun Any?.equalsAny(items: Iterable<Any?>): Boolean {
+    for (item in items) if (this == item) return true
+    return false
+}
+
+fun Any?.equalsNone(vararg items: Any?): Boolean {
+    for (item in items) if (this == item) return false
+    return true
+}
+
 infix fun Any?.equalsNone(items: Iterable<Any?>) = !equalsAny(items)
 
-fun Any?.equalsAll(vararg items: Any?) = equalsAll(items.asIterable())
+fun Any?.equalsAll(vararg items: Any?): Boolean {
+    for (item in items) if (this != item) return false
+    return true
+}
+
 infix fun Any?.equalsAll(items: Iterable<Any?>): Boolean {
-	for (item in items) if (this != item) return false; return true
+    for (item in items) if (this != item) return false
+    return true
 }
