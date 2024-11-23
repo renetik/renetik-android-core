c88bc33c770d5b9c5d6bc81a4b61a357c52f12b4
Rene Dohan <dohan.rene@email.com>
2024-11-23 14:39:47 +0800

    Make toast safe to run from  other thread..

--- library/src/main/java/renetik/android/core/extensions/content/Context+Toast.kt
+++ library/src/main/java/renetik/android/core/extensions/content/Context+Toast.kt
@@ -1,13 +1,15 @@
 package renetik.android.core.extensions.content
 
-//import renetik.android.core.lang.CSEnvironment.app
 import android.content.Context
 import android.widget.Toast.LENGTH_LONG
 import android.widget.Toast.LENGTH_SHORT
 import android.widget.Toast.makeText
 import renetik.android.core.extensions.content.CSToastLength.LongTime
 import renetik.android.core.extensions.content.CSToastLength.ShortTime
+import renetik.android.core.java.lang.isThreadMain
 import renetik.android.core.lang.CSEnvironment.app
+import renetik.android.core.lang.CSHandler.mainHandler
+import renetik.android.core.lang.send
 
 object CSToast {
     fun toast(text: String) = app.toast(text)
@@ -16,8 +18,10 @@ object CSToast {
 
 fun Context.toast(text: String) = toast(text, LongTime)
 
-fun Context.toast(text: String, time: CSToastLength = ShortTime) =
-    makeText(this, text, time.value).show()
+fun Context.toast(text: String, time: CSToastLength = ShortTime) {
+    fun toast() = makeText(this, text, time.value).show()
+    if (isThreadMain) toast() else mainHandler.send(::toast)
+}
 
 enum class CSToastLength(val value: Int) {
     LongTime(LENGTH_LONG), ShortTime(LENGTH_SHORT)
