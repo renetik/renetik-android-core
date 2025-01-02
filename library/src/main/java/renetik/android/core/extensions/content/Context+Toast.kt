7494044d9ef97356169a2ac080b5a6cb8fdce6c7
Rene Dohan <dohan.rene@email.com>
2025-01-02 17:37:15 +0800

    top toast support...

--- library/src/main/java/renetik/android/core/extensions/content/Context+Toast.kt
+++ library/src/main/java/renetik/android/core/extensions/content/Context+Toast.kt
@@ -1,28 +1,43 @@
 package renetik.android.core.extensions.content
 
 import android.content.Context
+import android.view.Gravity.CENTER_HORIZONTAL
+import android.view.Gravity.TOP
 import android.widget.Toast.LENGTH_LONG
 import android.widget.Toast.LENGTH_SHORT
 import android.widget.Toast.makeText
-import renetik.android.core.extensions.content.CSToastLength.LongTime
-import renetik.android.core.extensions.content.CSToastLength.ShortTime
+import renetik.android.core.extensions.content.CSToastLocation.Bottom
+import renetik.android.core.extensions.content.CSToastLocation.Top
+import renetik.android.core.extensions.content.CSToastTime.ShortTime
 import renetik.android.core.java.lang.isThreadMain
+import renetik.android.core.kotlin.primitives.dp
 import renetik.android.core.lang.CSEnvironment.app
 import renetik.android.core.lang.CSHandler.mainHandler
 import renetik.android.core.lang.send
 
+
 object CSToast {
     fun toast(text: String) = app.toast(text)
-    fun toast(text: String, time: CSToastLength = ShortTime) = app.toast(text, time)
+    fun toast(text: String, time: CSToastTime = ShortTime) =
+        app.toast(text, time)
 }
 
-fun Context.toast(text: String) = toast(text, LongTime)
+enum class CSToastLocation { Bottom, Top }
 
-fun Context.toast(text: String, time: CSToastLength = ShortTime) {
-    fun toast() = makeText(this, text, time.value).show()
+enum class CSToastTime(val value: Int) {
+    LongTime(LENGTH_LONG), ShortTime(LENGTH_SHORT)
+}
+
+fun Context.toast(text: String) = toast(text, time = ShortTime)
+
+fun Context.toast(
+    text: String,
+    time: CSToastTime = ShortTime,
+    location: CSToastLocation = Bottom,
+) {
+    fun toast() = makeText(this, text, time.value).apply {
+        if (location == Top) setGravity(TOP or CENTER_HORIZONTAL, 0, 100.dp)
+    }.show()
     if (isThreadMain) toast() else mainHandler.send(::toast)
 }
 
-enum class CSToastLength(val value: Int) {
-    LongTime(LENGTH_LONG), ShortTime(LENGTH_SHORT)
-}
