448248b08cd6205110a805164c483f03d9c0679e
Rene Dohan <dohan.rene@email.com>
2025-01-15 15:15:52 +0800

    ...

--- library/src/main/java/renetik/android/core/java/lang/CSThread.kt
+++ library/src/main/java/renetik/android/core/java/lang/CSThread.kt
@@ -3,12 +3,9 @@ package renetik.android.core.java.lang
 import java.lang.Thread.sleep
 
 object CSThread {
-    val currentThread: Thread get() = Thread.currentThread()
-
-    fun sleepNano(value: Long) {
-        val millis = value / 1_000_000
-        val nanos = (value % 1_000_000).toInt()
+    fun sleepNano(nanoseconds: Long) {
+        val millis = nanoseconds / 1_000_000
+        val nanos = (nanoseconds % 1_000_000).toInt()
         sleep(millis, nanos)
     }
-
 }
