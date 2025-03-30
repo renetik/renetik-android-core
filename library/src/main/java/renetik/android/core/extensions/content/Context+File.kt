ab607561cd5b13a4392d408fa21382b04850c755
Rene Dohan <dohan.rene@gmail.com>
2025-03-30 11:38:56 +0800

    Refactor: Rename temporary file and folder creation functions
    Rename `createTempFile` to `temporaryFile` and `createTempFolder` to `temporaryFolder` to better reflect their purpose. These functions are used to create files and folders in the application's cache directory for temporary operations. The updated names provide clarity and consistency.
    Update all the usages of renamed functions.

--- library/src/main/java/renetik/android/core/extensions/content/Context+File.kt
+++ library/src/main/java/renetik/android/core/extensions/content/Context+File.kt
@@ -4,10 +4,10 @@ import android.content.Context
 import java.io.File
 import java.util.UUID
 
-fun Context.createTempFile(extension: String? = null): File =
+fun Context.temporaryFile(extension: String? = null): File =
     File.createTempFile(applicationLabel, extension?.let { ".$it" }, cacheDir)
 
-fun Context.createTempFolder(): File {
+fun Context.temporaryFolder(): File {
     val uniqueName = "temp_folder_${UUID.randomUUID()}"
     val tempFolder = File(cacheDir, uniqueName)
     if (!tempFolder.exists()) tempFolder.mkdir()
