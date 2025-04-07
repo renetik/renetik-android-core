85550b0865a06856ceb641b45362e70e2da02522
Rene Dohan <dohan.rene@gmail.com>
2025-04-07 22:39:07 +0800

    Refactor: Move `openApplicationSettings` to `Context` extension
    This commit moves the `openApplicationSettings` function from `InstrumentsActivityView` to a `Context` extension for better code organization and reusability. This allows any `Context` to open the application's settings page. The function is now used in `InstrumentsActivityView` to request permissions.

--- library/src/main/java/renetik/android/core/extensions/content/Context+Activity.kt
+++ library/src/main/java/renetik/android/core/extensions/content/Context+Activity.kt
@@ -3,7 +3,9 @@ package renetik.android.core.extensions.content
 import android.content.Context
 import android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.reflect.KClass
 
@@ -22,4 +24,11 @@ fun <Activity : AppCompatActivity> Context.startActivity(
     for ((key, value) in extras) intent.putExtra(key, value)
     intent.addFlags(FLAG_ACTIVITY_NEW_TASK or FLAG_ACTIVITY_CLEAR_TASK)
     startActivity(intent, options)
+}
+
+fun Context.openApplicationSettings() {
+    val intent = android.content.Intent(ACTION_APPLICATION_DETAILS_SETTINGS)
+    intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
+    intent.data = Uri.fromParts("package", packageName, null)
+    startActivity(intent)
 }
