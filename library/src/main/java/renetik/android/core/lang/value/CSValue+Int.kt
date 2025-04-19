9b2fa408727b74e9a1c3342441465a30b8890969
Rene Dohan <dohan.rene@gmail.com>
2025-04-20 00:52:00 +0800

    ``` Refactor: Implement CSValue<Int> compareTo operator with CSValue<Int>
    Add a compareTo operator that allows comparison of CSValue<Int> with another CSValue<Int>.
    ```

--- library/src/main/java/renetik/android/core/lang/value/CSValue+Int.kt
+++ library/src/main/java/renetik/android/core/lang/value/CSValue+Int.kt
@@ -4,3 +4,4 @@ operator fun Int.plus(value: CSValue<Int>): Int = this + value.value
 operator fun CSValue<Int>.plus(value: CSValue<Int>): Int = value.value + value.value
 operator fun CSValue<Int>.plus(value: Int): Int = this.value + value
 operator fun CSValue<Int>.compareTo(value: Int): Int = this.value.compareTo(value)
+operator fun CSValue<Int>.compareTo(value: CSValue<Int>): Int = this.value.compareTo(value.value)
