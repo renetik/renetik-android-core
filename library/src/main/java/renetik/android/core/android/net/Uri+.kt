b41619b2243b1bd753d06ba51172b464d452e74b
Rene Dohan <dohan.rene@gmail.com>
2025-05-14 23:13:04 +0800

    library: Add uri helpers
    Added `toCacheUploadFile()` and `toUploadImageFileUri()` for Uri to File conversions.

---
+++ library/src/main/java/renetik/android/core/android/net/Uri+.kt
@@ -0,0 +1,29 @@
+package renetik.android.core.android.net
+
+import android.net.Uri
+import androidx.core.net.toUri
+import renetik.android.core.base.CSApplication.Companion.app
+import renetik.android.core.kotlin.unexpected
+import java.io.File
+import java.io.File.createTempFile
+import java.io.FileOutputStream
+
+fun Uri.toCacheUploadFile(): File {
+    val inputStream = app.contentResolver.openInputStream(this) ?: unexpected()
+    val tempFile = createTempFile("upload_", ".jpg", app.cacheDir)
+    FileOutputStream(tempFile).use { inputStream.copyTo(it) }
+    return tempFile
+}
+
+fun Uri.toUploadImageFileUri(): Uri = when (scheme) {
+    "file" -> this
+    "content" -> createTempUploadImageFile().also { file ->
+        app.contentResolver.openInputStream(this)?.use { input ->
+            file.outputStream().use(input::copyTo)
+        }
+    }.toUri()
+    else -> unexpected()
+}
+
+fun createTempUploadImageFile() = createTempFile("upload_image_", ".jpg", app.cacheDir)
+
