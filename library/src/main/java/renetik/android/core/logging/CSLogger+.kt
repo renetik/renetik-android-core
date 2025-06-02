5eec95d2434f73f789255c5498746d9eadbaa362
Rene Dohan <dohan.rene@gmail.com>
2025-06-03 00:09:41 +0800

    CSLogger: Inline `isEnabled` and `isDisabled` methods
    The `isEnabled` and `isDisabled` methods in `CSLogger` have been inlined for potential performance improvements by reducing function call overhead.

--- library/src/main/java/renetik/android/core/logging/CSLogger+.kt
+++ library/src/main/java/renetik/android/core/logging/CSLogger+.kt
@@ -1,5 +1,7 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package renetik.android.core.logging
 
-fun CSLogger.isEnabled(level: CSLogLevel) = !isDisabled(level)
+inline fun CSLogger.isEnabled(level: CSLogLevel) = !isDisabled(level)
 
-fun CSLogger.isDisabled(level: CSLogLevel) = level.ordinal < this.level.ordinal
+inline fun CSLogger.isDisabled(level: CSLogLevel) = level.ordinal < this.level.ordinal
