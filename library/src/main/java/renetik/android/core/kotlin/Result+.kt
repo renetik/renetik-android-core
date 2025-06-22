120d113274e86913dc82bd71765c17745bfef093
Rene Dohan <dohan.rene@gmail.com>
2025-06-22 20:39:44 +0800

    Rename onFailureOf overloads for distinct JVM names
    The `onFailureOf` function was overloaded for `Error` and `Throwable` types.
    This change adds `@JvmName` annotations to provide distinct names for these overloads at the JVM level, resolving potential conflicts.
    
    - `onFailureOf(onFailure: (Error) -> Unit)` is now `onFailureOfError`
    - `onFailureOf(onFailure: (Throwable) -> Unit)` is now `onFailureOfThrowable`

--- library/src/main/java/renetik/android/core/kotlin/Result+.kt
+++ library/src/main/java/renetik/android/core/kotlin/Result+.kt
@@ -1,9 +1,11 @@
 package renetik.android.core.kotlin
 
+@JvmName("onFailureOfError")
 inline fun <reified T : Error> Result<*>.onFailureOf(onFailure: (T) -> Unit) = apply {
     exceptionOrNull()?.also { if (it is T) onFailure(it) }
 }
 
+@JvmName("onFailureOfThrowable")
 inline fun <reified T : Throwable> Result<*>.onFailureOf(onFailure: (T) -> Unit) = apply {
     exceptionOrNull()?.also { if (it is T) onFailure(it) }
 }
