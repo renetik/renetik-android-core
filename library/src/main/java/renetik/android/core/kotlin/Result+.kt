24e40e372d838de2a0a3775ac384e3c1d31b3d4c
Rene Dohan <dohan.rene@gmail.com>
2025-06-22 19:46:03 +0800

    Core: Add Result.onFailureOf overload for Throwable

--- library/src/main/java/renetik/android/core/kotlin/Result+.kt
+++ library/src/main/java/renetik/android/core/kotlin/Result+.kt
@@ -3,3 +3,7 @@ package renetik.android.core.kotlin
 inline fun <reified T : Error> Result<*>.onFailureOf(onFailure: (T) -> Unit) = apply {
     exceptionOrNull()?.also { if (it is T) onFailure(it) }
 }
+
+inline fun <reified T : Throwable> Result<*>.onFailureOf(onFailure: (T) -> Unit) = apply {
+    exceptionOrNull()?.also { if (it is T) onFailure(it) }
+}
