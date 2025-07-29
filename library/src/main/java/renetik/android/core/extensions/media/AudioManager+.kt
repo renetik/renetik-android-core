7f40294a7d0275e8e8829ed7691f6c1b4a0a9ea3
Rene Dohan <dohan.rene@gmail.com>
2025-07-29 21:26:00 +0800

    Add `speakerDevice` property to `AudioManager`
    This commit introduces a new extension property `speakerDevice` to `AudioManager`.
    It returns the first output device that is a built-in speaker, or `null` if no such device is found.

--- library/src/main/java/renetik/android/core/extensions/media/AudioManager+.kt
+++ library/src/main/java/renetik/android/core/extensions/media/AudioManager+.kt
@@ -1,5 +1,7 @@
 package renetik.android.core.extensions.media
 
+import android.media.AudioDeviceInfo
+import android.media.AudioDeviceInfo.TYPE_BUILTIN_SPEAKER
 import android.media.AudioManager
 import android.media.AudioManager.PROPERTY_OUTPUT_FRAMES_PER_BUFFER
 import android.media.AudioManager.PROPERTY_OUTPUT_SAMPLE_RATE
@@ -12,3 +14,7 @@ val AudioManager.outputFramesPerBuffer: Int
 val AudioManager.outputSampleRate: Int
     get() = getProperty(PROPERTY_OUTPUT_SAMPLE_RATE)
         ?.asInt().takeUnless { it == 0 } ?: 44100
+
+val AudioManager.speakerDevice: AudioDeviceInfo?
+    get() = getDevices(AudioManager.GET_DEVICES_OUTPUTS)
+        .firstOrNull { it.type == TYPE_BUILTIN_SPEAKER }
