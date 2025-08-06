4677a06bad19dd7910e1f65803e86b68b95d2eb7
Rene Dohan <dohan.rene@gmail.com>
2025-08-06 11:02:48 +0800

    Refactor: Inline Rect extension functions and add contains operator
    This commit refactors the extension functions for `android.graphics.Rect` to be `inline` for potential performance improvements.
    
    Additionally, it introduces:
    - A `@file:Suppress("NOTHING_TO_INLINE")` annotation.
    - An `inline operator fun Rect?.contains(p: Point): Boolean` extension function that checks if a `Point` is within the nullable `Rect`.

--- library/src/main/java/renetik/android/core/extensions/graphics/Rect+.kt
+++ library/src/main/java/renetik/android/core/extensions/graphics/Rect+.kt
@@ -1,17 +1,21 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package renetik.android.core.extensions.graphics
 
+import android.graphics.Point
 import android.graphics.Rect
+import renetik.android.core.kotlin.primitives.isTrue
 import renetik.android.core.math.CSPoint
 import kotlin.math.max
 import kotlin.math.min
 
-val Rect.width get() = width()
+inline val Rect.width get() = width()
 
-val Rect.height get() = height()
+inline val Rect.height get() = height()
 
-val Rect.debugString get() = "left:$left top:$top width:$width height:$height"
+inline val Rect.debugString get() = "left:$left top:$top width:$width height:$height"
 
-fun Rect.load(start: CSPoint<Float>, end: CSPoint<Float>) = apply {
+inline fun Rect.load(start: CSPoint<Float>, end: CSPoint<Float>) = apply {
     val left = min(start.x, end.x)
     val top = min(start.y, end.y)
     val right = max(start.x, end.x)
@@ -19,14 +23,16 @@ fun Rect.load(start: CSPoint<Float>, end: CSPoint<Float>) = apply {
     set(left.toInt(), top.toInt(), right.toInt(), bottom.toInt())
 }
 
-fun Rect.clear() = set(0, 0, 0, 0)
+inline fun Rect.clear() = set(0, 0, 0, 0)
 
-fun Rect.offsetToNewLeft(value: Int) = offsetTo(value, top)
+inline fun Rect.offsetToNewLeft(value: Int) = offsetTo(value, top)
 
-fun Rect.offsetToNewRight(value: Int) = offsetTo(value - width, top)
+inline fun Rect.offsetToNewRight(value: Int) = offsetTo(value - width, top)
 
-fun Rect.offsetToNewTop(value: Int) = offsetTo(left, value)
+inline fun Rect.offsetToNewTop(value: Int) = offsetTo(left, value)
 
-fun Rect.offsetToNewBottom(value: Int) = offsetTo(left, value - height)
+inline fun Rect.offsetToNewBottom(value: Int) = offsetTo(left, value - height)
 
-fun Rect.copy(): Rect = Rect(this)
+inline fun Rect.copy(): Rect = Rect(this)
+
+inline operator fun Rect?.contains(p: Point): Boolean = this?.contains(p.x, p.y).isTrue
