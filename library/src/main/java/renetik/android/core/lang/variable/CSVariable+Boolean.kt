e799559a490a5f3b0d4753b7596b812456dd4e5c
Rene Dohan <dohan.rene@gmail.com>
2025-08-13 19:06:17 +0800

    Refactor: Use `run` instead of `apply` in `CSVariable<Boolean>` extensions
    The `setTrue` and `setFalse` extension functions for `CSVariable<Boolean>` have been changed to use `run` instead of `apply`. This change is made because `apply` returns the context object (the `CSVariable` itself), which is not needed in this case as the functions' purpose is to modify the variable's value and not to chain further operations on the variable. Using `run` more accurately reflects the intent of these functions.
    
    The `toggle` function remains unchanged as it correctly uses `apply` to allow for potential chaining.

--- library/src/main/java/renetik/android/core/lang/variable/CSVariable+Boolean.kt
+++ library/src/main/java/renetik/android/core/lang/variable/CSVariable+Boolean.kt
@@ -1,5 +1,5 @@
 package renetik.android.core.lang.variable
 
-fun CSVariable<Boolean>.setTrue() = apply { this.value = true }
-fun CSVariable<Boolean>.setFalse() = apply { this.value = false }
+fun CSVariable<Boolean>.setTrue() = run { this.value = true }
+fun CSVariable<Boolean>.setFalse() = run { this.value = false }
 fun CSVariable<Boolean>.toggle() = apply { value = !value }
