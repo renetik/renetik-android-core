b50cd4c6b62ca1e0efc5f67ca8c494e590a549ae
Rene Dohan <dohan.rene@gmail.com>
2025-10-19 13:57:47 +0800

    Refactor: Rename `shutdownAndWait` to `shutdownWaiting`
    This commit renames the `ExecutorService` extension function `shutdownAndWait` to `shutdownWaiting` to better reflect its behavior and avoid confusion with a similarly named function.
    
    The key changes include:
    - Renaming `shutdownAndWait(timeout: Duration)` to `shutdownWaiting(timeout: Duration)` in `ExecutorService+.kt`.
    - Updating the call site in `InstApplication.kt` to use the new function name, `shutdownWaiting()`, during the application shutdown process.

--- library/src/main/java/renetik/android/core/java/util/concurrent/ExecutorService+.kt
+++ library/src/main/java/renetik/android/core/java/util/concurrent/ExecutorService+.kt
@@ -5,13 +5,16 @@ import renetik.android.core.base.CSApplication.Companion.app
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.TimeUnit
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 fun ExecutorService.shutdownAndWait(second: Int = 10) {
     shutdown()
     awaitTermination(second.toLong(), TimeUnit.SECONDS)
 }
 
-suspend fun ExecutorService.shutdownAndWait(timeout: Duration): Boolean = app.IO {
+suspend fun ExecutorService.shutdownWaiting(
+    timeout: Duration = 10.seconds
+): Boolean = app.IO {
     shutdown()
     awaitTermination(timeout.inWholeMilliseconds, TimeUnit.MILLISECONDS)
 }
