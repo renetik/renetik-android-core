2f15b02686dfb5429778e414b39fd66de6e1be33
Rene Dohan <dohan.rene@gmail.com>
2025-11-25 18:39:02 +0100

    feat(CSVariable): Add thread-safe toggle for CSSafeVariable
    Adds a `toggle()` extension function for `CSSafeVariable<Boolean>`.
    This implementation uses a `compareAndSet` loop to ensure thread-safe toggling of the boolean value.

--- library/src/main/java/renetik/android/core/lang/variable/CSVariable+Boolean.kt
+++ library/src/main/java/renetik/android/core/lang/variable/CSVariable+Boolean.kt
@@ -3,3 +3,10 @@ package renetik.android.core.lang.variable
 fun CSVariable<Boolean>.setTrue() = run { this.value = true }
 fun CSVariable<Boolean>.setFalse() = run { this.value = false }
 fun CSVariable<Boolean>.toggle() = apply { value = !value }
+
+fun CSSafeVariable<Boolean>.toggle() = apply {
+    while (true) {
+        val current = value
+        if (compareAndSet(current, !current)) return@apply
+    }
+}
