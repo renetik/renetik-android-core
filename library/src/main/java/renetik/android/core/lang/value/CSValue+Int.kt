3dbe997b5780e357534302cc4b2f5710082dd4f8
Rene Dohan <dohan.rene@gmail.com>
2025-12-05 19:04:40 +0100

    Refactor: Add operator functions for CSValue numeric types
    Expanded the operator functions for `CSValue<Int>`, `CSValue<Long>`, `CSValue<Float>`, and `CSValue<Double>`.
    
    This change introduces overloads for `plus`, `minus`, `times`, and `div` operators to allow arithmetic operations between `CSValue` types and between `CSValue` and standard numeric types (Int, Long, Float, Double).
    
    `@JvmName` annotations have been added to prevent JVM signature clashes for overloaded functions and to provide clearer names.

--- library/src/main/java/renetik/android/core/lang/value/CSValue+Int.kt
+++ library/src/main/java/renetik/android/core/lang/value/CSValue+Int.kt
@@ -2,29 +2,87 @@
 
 package renetik.android.core.lang.value
 
+import kotlin.jvm.JvmName
+
+@JvmName("intPlusCSValueInt")
 inline operator fun Int.plus(value: CSValue<Int>): Int = this + value.value
+
+@JvmName("intPlusCSValueFloat")
 inline operator fun Int.plus(value: CSValue<Float>): Float = this + value.value
+
+@JvmName("csValueIntPlusCSValueInt")
 inline operator fun CSValue<Int>.plus(value: CSValue<Int>): Int = value.value + value.value
+
+@JvmName("csValueIntPlusCSValueLong")
+inline operator fun CSValue<Int>.plus(value: CSValue<Long>): Long = value.value + value.value
+
+@JvmName("csValueIntPlusCSValueFloat")
+inline operator fun CSValue<Int>.plus(value: CSValue<Float>): Float = value.value + value.value
+
+@JvmName("csValueIntPlusCSValueDouble")
+inline operator fun CSValue<Int>.plus(value: CSValue<Double>): Double = value.value + value.value
+
 inline operator fun CSValue<Int>.plus(value: Int): Int = this.value + value
 inline operator fun CSValue<Int>.plus(value: Float): Float = this.value + value
 
+@JvmName("longPlusCSValueLong")
+inline operator fun Long.plus(value: CSValue<Long>): Long = this + value.value
+
+@JvmName("longPlusCSValueInt")
+inline operator fun Long.plus(value: CSValue<Int>): Long = this + value.value
+
+@JvmName("longPlusCSValueFloat")
+inline operator fun Long.plus(value: CSValue<Float>): Float = this + value.value
+
+@JvmName("csValueLongPlusCSValueLong")
+inline operator fun CSValue<Long>.plus(value: CSValue<Long>): Long = value.value + value.value
+
+@JvmName("csValueLongDivFloat")
+inline operator fun CSValue<Long>.div(value: Float): Float = value / value
+
+@JvmName("intMinusCSValueInt")
 inline operator fun Int.minus(value: CSValue<Int>): Int = this - value.value
+
+@JvmName("intMinusCSValueFloat")
 inline operator fun Int.minus(value: CSValue<Float>): Float = this - value.value
+
 inline operator fun CSValue<Int>.minus(value: Int): Int = this.value - value
 inline operator fun CSValue<Float>.minus(value: Int): Float = this.value - value
 inline operator fun CSValue<Int>.minus(value: Float): Float = this.value - value
+
+@JvmName("csValueIntMinusCSValueInt")
 inline operator fun CSValue<Int>.minus(value: CSValue<Int>): Int = this.value - value.value
 
+@JvmName("intTimesCSValueInt")
 inline operator fun Int.times(value: CSValue<Int>): Int = this * value.value
+
+@JvmName("intTimesCSValueFloat")
 inline operator fun Int.times(value: CSValue<Float>): Float = this * value.value
+
+@JvmName("intTimesCSValueLong")
+inline operator fun Int.times(value: CSValue<Long>): Long = this * value.value
+
+@JvmName("intTimesCSValueDouble")
+inline operator fun Int.times(value: CSValue<Double>): Double = this * value.value
+
 inline operator fun CSValue<Int>.times(value: Int): Int = this.value * value
 inline operator fun CSValue<Int>.times(value: Float): Float = this.value * value
+
+@JvmName("csValueIntTimesCSValueInt")
 inline operator fun CSValue<Int>.times(value: CSValue<Int>): Int = this.value * value.value
+
+@JvmName("csValueIntTimesCSValueFloat")
 inline operator fun CSValue<Int>.times(value: CSValue<Float>): Float = this.value * value.value
 
+@JvmName("intDivCSValueInt")
 inline operator fun Int.div(value: CSValue<Int>): Int = this / value.value
+
+@JvmName("intDivCSValueFloat")
 inline operator fun Int.div(value: CSValue<Float>): Float = this / value.value
+
 inline operator fun CSValue<Int>.div(value: Int): Int = this.value / value
+
+@JvmName("csValueIntDivFloat")
 inline operator fun CSValue<Int>.div(value: Float): Float = this.value / value
 
 inline operator fun CSValue<Int>.compareTo(value: Int): Int =
@@ -33,11 +91,12 @@ inline operator fun CSValue<Int>.compareTo(value: Int): Int =
 inline operator fun CSValue<Int>.compareTo(value: CSValue<Int>): Int =
     this.value.compareTo(value.value)
 
-@JvmName("CSValueFloatCompareToInt")
+@JvmName("csValueFloatCompareToInt")
 inline operator fun CSValue<Float>.compareTo(value: Int): Int =
     this.value.compareTo(value)
 
-@JvmName("CSValueFloatCompareToCSValueInt")
+@JvmName("csValueFloatCompareToCSValueInt")
 inline operator fun CSValue<Float>.compareTo(value: CSValue<Int>): Int =
     this.value.compareTo(value.value)
+
 
