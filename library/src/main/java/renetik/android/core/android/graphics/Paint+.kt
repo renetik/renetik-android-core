45ef7403435e7d7974df965b21dc7fefbeecdb6f
Rene Dohan <dohan.rene@gmail.com>
2025-12-09 18:51:37 +0800

    style: Inline Paint helper functions
    This commit adds the `inline` keyword to the `Paint` factory and `clone` extension functions. This optimization improves performance by inlining the function calls at the call site.

--- library/src/main/java/renetik/android/core/android/graphics/Paint+.kt
+++ library/src/main/java/renetik/android/core/android/graphics/Paint+.kt
@@ -2,8 +2,8 @@ package renetik.android.core.android.graphics
 
 import android.graphics.Paint
 
-fun Paint(function: (Paint).() -> Unit): Paint =
+inline fun Paint(function: (Paint).() -> Unit): Paint =
     Paint(Paint.ANTI_ALIAS_FLAG).also(function)
 
-fun Paint.clone(function: (Paint).() -> Unit): Paint =
+inline fun Paint.clone(function: (Paint).() -> Unit): Paint =
     Paint(Paint.ANTI_ALIAS_FLAG).also { it.set(this); function(it) }
