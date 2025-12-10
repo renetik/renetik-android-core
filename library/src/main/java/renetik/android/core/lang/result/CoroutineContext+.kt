e8daa647b55c7c38c1f77e42fd55720006755b84
Rene Dohan <dohan.rene@gmail.com>
2025-12-10 14:34:05 +0800

    refactor: Remove `limitedParallelism` extension from `CoroutineContext`
    This commit removes the unused `limitedParallelism` extension function from `CoroutineContext+.kt`.

--- library/src/main/java/renetik/android/core/lang/result/CoroutineContext+.kt
+++ library/src/main/java/renetik/android/core/lang/result/CoroutineContext+.kt
@@ -1,16 +1,8 @@
 package renetik.android.core.lang.result
 
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineName
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.withContext
-import kotlin.coroutines.ContinuationInterceptor
 import kotlin.coroutines.CoroutineContext
-
-@OptIn(ExperimentalCoroutinesApi::class)
-fun CoroutineContext.limitedParallelism(parallelism: Int) =
-    (this[ContinuationInterceptor] as CoroutineDispatcher)
-        .limitedParallelism(parallelism)
 
 fun CoroutineContext.named(name: String): CoroutineContext =
     this + CoroutineName(name)
