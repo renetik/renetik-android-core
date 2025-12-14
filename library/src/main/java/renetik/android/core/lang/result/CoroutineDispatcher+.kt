765f269e04cb239e93c10712460e5c3c6422967e
Rene Dohan <dohan.rene@gmail.com>
2025-12-14 17:06:03 +0800

    refactor: Remove unused `EmptyDispatcher`
    This commit removes the unused `EmptyDispatcher` object and other commented-out code from `CoroutineDispatcher+.kt`.

--- library/src/main/java/renetik/android/core/lang/result/CoroutineDispatcher+.kt
+++ library/src/main/java/renetik/android/core/lang/result/CoroutineDispatcher+.kt
@@ -1,21 +1,14 @@
 package renetik.android.core.lang.result
 
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.ExecutorCoroutineDispatcher
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.currentCoroutineContext
-import kotlinx.coroutines.newSingleThreadContext
-import kotlin.coroutines.CoroutineContext
 
-object EmptyDispatcher : CoroutineDispatcher() {
-    override fun isDispatchNeeded(context: CoroutineContext): Boolean = false
-    override fun dispatch(context: CoroutineContext, block: Runnable) = block.run()
-}
+//object EmptyDispatcher : CoroutineDispatcher() {
+//    override fun isDispatchNeeded(context: CoroutineContext): Boolean = false
+//    override fun dispatch(context: CoroutineContext, block: Runnable) = block.run()
+//}
+//
 
 @OptIn(ExperimentalStdlibApi::class)
 suspend fun currentDispatcher(): CoroutineDispatcher? =
     currentCoroutineContext()[CoroutineDispatcher]
-
-//@OptIn(DelicateCoroutinesApi::class, ExperimentalCoroutinesApi::class)
-//val SingleThread: ExecutorCoroutineDispatcher = newSingleThreadContext("SingleThread")
