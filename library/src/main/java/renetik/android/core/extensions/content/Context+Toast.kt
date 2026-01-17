a09c99e1fbfe157693becf59abb062b8d6028e67
Rene Dohan <dohan.rene@gmail.com>
2026-01-17 20:10:12 +0800

    refactor: Change default toast duration to short
    This commit changes the default duration for toast messages from `LENGTH_LONG` to `LENGTH_SHORT`.
    
    - The default value for the `time` parameter in `CSToast.toast` and `Context.toast` is now `ShortTime`.
    - The order of enum values in `CSToastTime` has been changed to `ShortTime`, `LongTime`.
    - The unused import `renetik.android.core.extensions.content.CSToastTime.LongTime` has been removed.

--- library/src/main/java/renetik/android/core/extensions/content/Context+Toast.kt
+++ library/src/main/java/renetik/android/core/extensions/content/Context+Toast.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.widget.Toast
 import android.widget.Toast.LENGTH_LONG
 import android.widget.Toast.LENGTH_SHORT
-import renetik.android.core.extensions.content.CSToastTime.LongTime
 import renetik.android.core.extensions.content.CSToastTime.ShortTime
 import renetik.android.core.java.lang.isThreadMain
 import renetik.android.core.lang.CSEnvironment.app
@@ -12,19 +11,19 @@ import renetik.android.core.lang.CSHandler.mainHandler
 import renetik.android.core.lang.send
 
 object CSToast {
-    fun toast(text: String) =
-        app.toast(text)
-    fun toast(text: String, time: CSToastTime = LongTime) =
+    fun toast(text: String) = app.toast(text)
+
+    fun toast(text: String, time: CSToastTime = ShortTime) =
         app.toast(text, time)
 }
 
 enum class CSToastTime(val value: Int) {
-    LongTime(LENGTH_LONG), ShortTime(LENGTH_SHORT)
+    ShortTime(LENGTH_SHORT), LongTime(LENGTH_LONG)
 }
 
-fun Context.toast(text: String) = toast(text, time = LongTime)
+fun Context.toast(text: String) = toast(text, time = ShortTime)
 
-fun Context.toast(text: String, time: CSToastTime = LongTime) {
+fun Context.toast(text: String, time: CSToastTime = ShortTime) {
     fun toast() = Toast.makeText(this, text, time.value).show()
     if (isThreadMain) toast() else mainHandler.send(::toast)
 }
