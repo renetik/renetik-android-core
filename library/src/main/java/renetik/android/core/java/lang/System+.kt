15ea9b80dab40fd64b636b2e4fcf861de0ff5d0a
Rene Dohan <dohan.rene@gmail.com>
2026-02-05 01:10:38 +0800

    Refactor: Replace `nanoTime` extension with direct `System.nanoTime()` calls
    *   Remove the deprecated `nanoTime` extension property from `renetik-android-core`.
    *   Update all call sites to use `System.nanoTime()` directly.
    *   Rename `trackFileKeyRecordFlush` to `trackFileKeyRecordSave` in `SamplerEngineTest` to align with API changes.

--- library/src/main/java/renetik/android/core/java/lang/System+.kt
+++ library/src/main/java/renetik/android/core/java/lang/System+.kt
@@ -5,7 +5,4 @@ import kotlin.time.Duration
 import kotlin.time.DurationUnit.MILLISECONDS
 import kotlin.time.toDuration
 
-@Deprecated("TODO: Not really useful, inline")
-val nanoTime: Long get() = System.nanoTime()
-
 val nowDuration: Duration get() = currentTimeMillis().toDuration(MILLISECONDS)
