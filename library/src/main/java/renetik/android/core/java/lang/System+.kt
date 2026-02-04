1cc7737bcc80d2f3aad33d3c97d1eed32c40a647
Rene Dohan <dohan.rene@gmail.com>
2026-02-04 10:16:18 +0800

    Deprecate `nanoTime` extension in `System+.kt`
    * Mark `nanoTime` as deprecated with a note to inline its usage.

--- library/src/main/java/renetik/android/core/java/lang/System+.kt
+++ library/src/main/java/renetik/android/core/java/lang/System+.kt
@@ -5,6 +5,7 @@ import kotlin.time.Duration
 import kotlin.time.DurationUnit.MILLISECONDS
 import kotlin.time.toDuration
 
+@Deprecated("TODO: Not really useful, inline")
 val nanoTime: Long get() = System.nanoTime()
 
 val nowDuration: Duration get() = currentTimeMillis().toDuration(MILLISECONDS)
