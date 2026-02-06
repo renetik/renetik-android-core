7c57f13772867391a647c5952b7f4e6aa5232b6c
Rene Dohan <dohan.rene@gmail.com>
2026-02-06 14:49:52 +0800

    Refactor CSLog to use inline wrappers for zero-allocation logging
    *   Refactor logging methods (`logVerbose`, `logDebug`, `logInfo`, etc.) into `inline` functions to avoid unnecessary string allocation and lambda creation when the log level is disabled.
    *   Introduce `printLog` as a non-inline internal anchor method to handle the actual logging implementation.
    *   Replace the `logImpl` based trace discovery with a more robust logic in `getTraceLine` that identifies the caller by skipping all `CSLog` internal stack frames.
    *   Update `Context.toast` extensions to be compatible with the new logging structure.
    *   Add `@file:Suppress("NOTHING_TO_INLINE", "unused")` and remove unused imports.

--- library/src/main/java/renetik/android/core/logging/CSLog.kt
+++ library/src/main/java/renetik/android/core/logging/CSLog.kt
@@ -1,4 +1,5 @@
 @file:JvmName("CSLog")
+@file:Suppress("NOTHING_TO_INLINE", "unused")
 
 package renetik.android.core.logging
 
@@ -6,7 +7,6 @@ import android.content.Context
 import renetik.android.core.extensions.content.toast
 import renetik.android.core.kotlin.CSUnexpectedException
 import renetik.android.core.kotlin.primitives.leaveEndOfLength
-import renetik.android.core.kotlin.then
 import renetik.android.core.kotlin.toShortString
 import renetik.android.core.lang.CSEnvironment.isDebug
 import renetik.android.core.lang.CSStringConstants.NewLine
@@ -15,14 +15,12 @@ import renetik.android.core.logging.CSLogLevel.Error
 import renetik.android.core.logging.CSLogLevel.Info
 import renetik.android.core.logging.CSLogLevel.Verbose
 import renetik.android.core.logging.CSLogLevel.Warn
-import renetik.android.core.logging.CSLogMessage.Companion.Empty
 import renetik.android.core.logging.CSLogMessage.Companion.message
 import renetik.android.core.logging.CSLogMessage.Companion.traceMessage
 import java.lang.System.currentTimeMillis
 import java.lang.Thread.currentThread
 import java.text.DateFormat.getDateTimeInstance
 
-//TODO: inline all
 object CSLog {
     var logger: CSLogger = CSPrintLogger()
 
@@ -30,134 +28,188 @@ object CSLog {
         this.logger = logger
     }
 
-    fun log(level: CSLogLevel) = then { logImpl(level) { message("") } }
-    fun log(level: CSLogLevel, function: () -> CSLogMessage) =
-        then { logImpl(level, function) }
+    // -----------------------------------------------------------------------
+    // INLINE WRAPPERS (Zero Allocation)
+    // -----------------------------------------------------------------------
+    inline fun log(level: CSLogLevel, crossinline function: () -> CSLogMessage) {
+        if (logger.isEnabled(level)) printLog(level, function())
+    }
 
-    fun logVerbose(any: Any? = null) = then { logImpl(Verbose) { message(any) } }
-    fun logVerbose(function: () -> String) =
-        then { logImpl(Verbose) { message(function()) } }
+    inline fun logVerbose(any: Any? = null) {
+        if (logger.isEnabled(Verbose)) printLog(Verbose, message(any))
+    }
 
-    @JvmStatic
-    fun logDebug() = then { logImpl(Debug) { message() } }
-
-    @JvmStatic
-    fun logDebug(any: Any?) = then { logImpl(Debug) { message(any) } }
-
-    @JvmStatic
-    fun logDebug(function: () -> String) = then { logImpl(Debug) { message(function()) } }
+    inline fun logVerbose(crossinline function: () -> String) {
+        if (logger.isEnabled(Verbose)) printLog(Verbose, message(function()))
+    }
 
     @JvmStatic
-    fun logDebug(throwable: Throwable, function: (() -> String)? = null) =
-        then { logImpl(Debug) { message(throwable, function?.invoke()) } }
+    inline fun logDebug(crossinline function: () -> String) {
+        if (logger.isEnabled(Debug)) printLog(Debug, message(function()))
+    }
 
     @JvmStatic
-    fun logDebugTrace(function: (() -> String)? = null) =
-        then { logImpl(Debug) { message(Throwable(), function?.invoke()) } }
+    inline fun logDebug(any: Any?) {
+        if (logger.isEnabled(Debug)) printLog(Debug, message(any))
+    }
 
     @JvmStatic
-    fun logInfo() = then { logImpl(Info) { message("") } }
+    fun logDebug() {
+        if (logger.isEnabled(Debug)) printLog(Debug, message())
+    }
 
     @JvmStatic
-    fun logInfo(any: Any?) = then { logImpl(Info) { message(any) } }
-    fun logInfo(throwable: Throwable, message: String) =
-        then { logImpl(Info) { message(throwable, message) } }
+    inline fun logDebug(throwable: Throwable, crossinline function: (() -> String)) {
+        if (logger.isEnabled(Debug)) printLog(Debug, message(throwable, function()))
+    }
 
     @JvmStatic
-    fun logInfo(function: () -> String) = then { logImpl(Info) { message(function()) } }
+    inline fun logDebug(throwable: Throwable) {
+        if (logger.isEnabled(Debug)) printLog(Debug,
+            message(throwable))
+    }
 
     @JvmStatic
-    fun logInfo(throwable: Throwable, function: (() -> String)? = null) =
-        then { logImpl(Info) { message(throwable, function?.invoke()) } }
+    inline fun logDebugTrace(noinline function: (() -> String)? = null) {
+        if (logger.isEnabled(Debug)) printLog(Debug,
+            message(Throwable(), function?.invoke()))
+    }
 
-    fun logInfoTrace(any: Any? = null, skip: Int = 0, length: Int = 5) =
-        then {
-            logImpl(Info) {
-                message(
-                    "$any\n" + Throwable().toShortString(skip, length)
-                )
-            }
+    @JvmStatic
+    inline fun logInfo() {
+        if (logger.isEnabled(Info)) printLog(Info, message(""))
+    }
+
+    @JvmStatic
+    inline fun logInfo(any: Any?) {
+        if (logger.isEnabled(Info)) printLog(Info, message(any))
+    }
+
+    @JvmStatic
+    inline fun logInfo(crossinline function: () -> String) {
+        if (logger.isEnabled(Info)) printLog(Info, message(function()))
+    }
+
+    @JvmStatic
+    inline fun logInfo(throwable: Throwable, message: String) {
+        if (logger.isEnabled(Info)) printLog(Info, message(throwable, message))
+    }
+
+    @JvmStatic
+    inline fun logInfo(throwable: Throwable, noinline function: (() -> String)? = null) {
+        if (logger.isEnabled(Info)) printLog(Info, message(throwable, function?.invoke()))
+    }
+
+    @JvmStatic
+    inline fun logInfoTrace(any: Any? = null, skip: Int = 0, length: Int = 5) {
+        if (logger.isEnabled(Info)) {
+            printLog(Info, message("$any\n" + Throwable().toShortString(skip, length)))
         }
-
-    fun logInfoTrace(function: () -> String) = then {
-        if (isDebug) logImpl(Info) { traceMessage(function()) }
-        else logImpl(Info) { message(Throwable(), function.invoke()) }
     }
 
-    fun logWarn() = then { logImpl(Warn) { message("") } }
-    fun logWarn(any: Any?) = then { logImpl(Warn) { message(any) } }
-    fun logWarn(function: () -> String) = then { logImpl(Warn) { message(function()) } }
-    fun logWarn(throwable: Throwable?, function: (() -> String)? = null) =
-        then { logImpl(Warn) { message(throwable, function?.invoke()) } }
-
-    fun logWarn(throwable: Throwable?, message: String?) =
-        then { logImpl(Warn) { message(throwable, message) } }
-
-    fun logWarnTrace(function: () -> String) = then {
-        if (isDebug) logImpl(Warn) { traceMessage(function()) }
-        else logImpl(Warn) { message(Throwable(), function.invoke()) }
-    }
-
-    fun logError() = then { logImpl(Error) { message("") } }
-    fun logError(message: String?) = then { logImpl(Error) { message(message) } }
-    fun logError(function: () -> String) = then { logImpl(Error) { message(function()) } }
-    fun logError(throwable: Throwable?) = then { logImpl(Error) { message(throwable) } }
-    fun logError(throwable: Throwable?, message: String?) =
-        then { logImpl(Error) { message(throwable, message) } }
-
-    fun logErrorTrace() = then { logImpl(Error) { message(Throwable()) } }
-
-    fun logErrorTrace(function: () -> String) =
-        then { logImpl(Error) { message(Throwable(), function.invoke()) } }
-
-    fun Context.logDebugToast(value: String) =
-        toast(Debug, logImpl(Debug) { message(value) })
-
-    fun Context.logDebugToast() = toast(Debug, logImpl(Debug) { message("") })
-
-    fun Context.logDebugStringToast(function: () -> String) =
-        toast(Debug, logImpl(Debug) { CSLogMessage(message = function()) })
-
-    fun Context.logDebugToast(function: () -> CSLogMessage) =
-        toast(Debug, logImpl(Debug, function))
-
-    fun Context.logInfoToast() = toast(Info, logImpl(Info) { message("") })
-    fun Context.logInfoToast(value: String) =
-        toast(Info, logImpl(Info) { message(value) })
-
-    fun Context.logInfoToast(function: () -> CSLogMessage) =
-        toast(Info, logImpl(Info, function))
-
-    fun Context.logWarnToast() = toast(Warn, logImpl(Warn) { message("") })
-    fun Context.logWarnToast(function: () -> CSLogMessage) =
-        toast(Warn, logImpl(Warn, function))
-
-    fun Context.logErrorToast() = toast(Error, logImpl(Error) { message("") })
-    fun Context.logErrorToast(function: () -> CSLogMessage) =
-        toast(Error, logImpl(Error, function))
-
-    private fun logImpl(
-        level: CSLogLevel, message: () -> CSLogMessage = { Empty }
-    ): Array<Any?>? {
-        if (logger.isEnabled(level)) message().let {
-            val text = createMessage(it.message)
-            when (level) {
-                Verbose -> logger.verbose(it.throwable, *text)
-                Debug -> logger.debug(it.throwable, *text)
-                Info -> logger.info(it.throwable, *text)
-                Warn -> logger.warn(it.throwable, *text)
-                Error -> logger.error(it.throwable, *text)
-                else -> CSUnexpectedException.unexpected()
-            }
-            return text
+    @JvmStatic
+    inline fun logInfoTrace(crossinline function: () -> String) {
+        if (logger.isEnabled(Info)) {
+            if (isDebug) printLog(Info, traceMessage(function()))
+            else printLog(Info, message(Throwable(), function()))
         }
-        return null
     }
 
-    private fun Context.toast(level: CSLogLevel, text: Array<Any?>?) {
-        if (logger.isEnabled(level)) toast(
-            "${text!![2]}".leaveEndOfLength(100).chunked(50)
+    @JvmStatic
+    inline fun logWarn() {
+        if (logger.isEnabled(Warn)) printLog(Warn, message(""))
+    }
+
+    @JvmStatic
+    inline fun logWarn(any: Any?) {
+        if (logger.isEnabled(Warn)) printLog(Warn, message(any))
+    }
+
+    @JvmStatic
+    inline fun logWarn(crossinline function: () -> String) {
+        if (logger.isEnabled(Warn)) printLog(Warn, message(function()))
+    }
+
+    @JvmStatic
+    inline fun logWarn(throwable: Throwable?, noinline function: (() -> String)? = null) {
+        if (logger.isEnabled(Warn)) printLog(Warn, message(throwable, function?.invoke()))
+    }
+
+    @JvmStatic
+    inline fun logWarn(throwable: Throwable?, message: String?) {
+        if (logger.isEnabled(Warn)) printLog(Warn, message(throwable, message))
+    }
+
+    @JvmStatic
+    inline fun logWarnTrace(crossinline function: () -> String) {
+        if (logger.isEnabled(Warn)) {
+            if (isDebug) printLog(Warn, traceMessage(function()))
+            else printLog(Warn, message(Throwable(), function()))
+        }
+    }
+
+    @JvmStatic
+    inline fun logError() {
+        if (logger.isEnabled(Error)) printLog(Error, message(""))
+    }
+
+    @JvmStatic
+    inline fun logError(message: String?) {
+        if (logger.isEnabled(Error)) printLog(Error, message(message))
+    }
+
+    @JvmStatic
+    inline fun logError(crossinline function: () -> String) {
+        if (logger.isEnabled(Error)) printLog(Error, message(function()))
+    }
+
+    @JvmStatic
+    inline fun logError(throwable: Throwable?) {
+        if (logger.isEnabled(Error)) printLog(Error, message(throwable))
+    }
+
+    @JvmStatic
+    inline fun logError(throwable: Throwable?, message: String?) {
+        if (logger.isEnabled(Error)) printLog(Error, message(throwable, message))
+    }
+
+    @JvmStatic
+    inline fun logErrorTrace() {
+        if (logger.isEnabled(Error)) printLog(Error, message(Throwable()))
+    }
+
+    @JvmStatic
+    inline fun logErrorTrace(crossinline function: () -> String) {
+        if (logger.isEnabled(Error)) printLog(Error, message(Throwable(), function()))
+    }
+
+    // -----------------------------------------------------------------------
+    // INTERNAL IMPLEMENTATION (The Anchor)
+    // -----------------------------------------------------------------------
+
+    /**
+     * This is the "Anchor" method. It is NOT inline.
+     * It will always appear in the stack trace, allowing us to find the caller reliably.
+     */
+    @PublishedApi
+    internal fun printLog(level: CSLogLevel, msg: CSLogMessage): Array<Any?> {
+        val text = createMessage(msg.message)
+        when (level) {
+            Verbose -> logger.verbose(msg.throwable, *text)
+            Debug -> logger.debug(msg.throwable, *text)
+            Info -> logger.info(msg.throwable, *text)
+            Warn -> logger.warn(msg.throwable, *text)
+            Error -> logger.error(msg.throwable, *text)
+            else -> CSUnexpectedException.unexpected()
+        }
+        return text
+    }
+
+    fun Context.toast(level: CSLogLevel, text: Array<Any?>?) {
+        if (text != null && logger.isEnabled(level)) {
+            toast("${text[2]}".leaveEndOfLength(100).chunked(50)
                 .joinToString { "$it$NewLine" })
+        }
     }
 
     private val timeFormat by lazy { getDateTimeInstance() }
@@ -170,11 +222,22 @@ object CSLog {
         }
     }
 
+    /**
+     * The first frame that isn't CSLog is the caller.
+     */
     private fun getTraceLine(): String {
         val stackTrace = currentThread().stackTrace
-        val index = stackTrace.indexOfFirst { it.methodName == "logImpl" } + 2
-        return stackTrace[index].let { element ->
-            "${element.className}$${element.methodName}(${element.fileName}:${element.lineNumber})"
+        val logClassName = CSLog::class.java.name
+        for (index in stackTrace.indices) {
+            val element = stackTrace[index]
+            val className = element.className
+            val isLoggerInternal = className.startsWith(logClassName) ||
+                    className == "java.lang.Thread" ||
+                    className == "dalvik.system.VMStack"
+            if (!isLoggerInternal) {
+                return "${element.className}$${element.methodName}(${element.fileName}:${element.lineNumber})"
+            }
         }
+        return "UnknownSource"
     }
 }
